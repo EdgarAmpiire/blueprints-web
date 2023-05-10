14699c053963a348e191e0257300bec0dba79919
edgarampiire <aedgar990@gmail.com>
2023-05-10 16:35:49 +0300

    home page - contact us

---
+++ src/features/contactUs/routes/ContactUs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { MainLayout } from "../../../components/layout/MainLayout";
+
+export const ContactUs = () => {
+  return (
+    <div>
+      <MainLayout page="contact-us">
+        <h1>ContactUs</h1>
+      </MainLayout>
+    </div>
+  );
+};
