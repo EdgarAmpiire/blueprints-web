6a0b1681370b8e472b7391ff7afc0e90baabae02
edgarampiire <aedgar990@gmail.com>
2023-05-10 16:22:24 +0300

    home page - about us

--- src/routes/public.jsx
+++ src/routes/public.jsx
@@ -1,8 +1,9 @@
+import { AboutUs } from "../features/aboutUs";
 import { Home } from "../features/home";
 import { Services } from "../features/services";
   
   export const publicRoutes = [
     {path: "/", element: <Home />},
     {path: "/services", element: <Services />},
-
+    {path: "/about-us", element: <AboutUs />},
   ];
