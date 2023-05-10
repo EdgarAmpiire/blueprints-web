d5ac8b61cbbda131bab1b35ec245ae832d77b561
edgarampiire <aedgar990@gmail.com>
2023-05-10 16:21:32 +0300

    home page - about us

--- src/features/aboutUs/routes/AboutUs.jsx
+++ src/features/aboutUs/routes/AboutUs.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import { MainLayout } from "../../../components/layout/MainLayout";
 
 export const AboutUs = () => {
-  return (<div>
-    <MainLayout page="about-us">
-
-    AboutUs
-    </MainLayout>
-    
-    </div>)
+  return (
+    <div>
+      <MainLayout page="about-us">AboutUs</MainLayout>
+    </div>
+  );
 };
