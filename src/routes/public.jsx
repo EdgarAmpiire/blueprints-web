4edd8b2b23f1dec6969559a49068002efba10e23
edgarampiire <aedgar990@gmail.com>
2023-05-10 16:08:20 +0300

    home page - services

--- src/routes/public.jsx
+++ src/routes/public.jsx
@@ -1,5 +1,8 @@
 import { Home } from "../features/home";
+import { Services } from "../features/services/routes/Services";
   
   export const publicRoutes = [
-    {path: "/", element: <Home />}
+    {path: "/", element: <Home />},
+    {path: "/services", element: <Home />},
+
   ];
