6159acc644a70d3eb147b4d707d240c7b6fc4bf0
edgarampiire <aedgar990@gmail.com>
2023-05-10 13:30:19 +0300

    home page - routes

--- src/App.js
+++ src/App.js
@@ -22,8 +22,15 @@ function App() {
               alt="Loading"
               className="h-32 w-auto mx-auto animate__animated animate__flipInX animate-bounce"
             /> */}
-           <h1 className="text-[#0055BA]"></h1>
+           <h1 className="text-[#0055BA]">
+
             Blueprint Strategy
+           </h1>
+
+           <span class="relative flex h-3 w-3">
+  <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+  <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+</span>
           </div>
         ) : (
           <div className="transition ease-in delay-150">
