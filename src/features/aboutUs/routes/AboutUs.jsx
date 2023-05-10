e306f647e7ba3b13a69e41bafd52685b3c5b2e76
edgarampiire <aedgar990@gmail.com>
2023-05-10 16:19:35 +0300

    home page - about us

---
+++ src/features/aboutUs/routes/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { MainLayout } from "../../../components/layout/MainLayout";
+
+export const AboutUs = () => {
+  return (<div>
+    <MainLayout page="about-us">
+
+    AboutUs
+    </MainLayout>
+    
+    </div>)
+};
