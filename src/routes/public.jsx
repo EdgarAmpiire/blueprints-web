799fbdc3ff1c942256ddb22d9729af591e138acb
edgarampiire <aedgar990@gmail.com>
2023-05-10 16:37:02 +0300

    home page - contact us

--- src/routes/public.jsx
+++ src/routes/public.jsx
@@ -1,4 +1,5 @@
 import { AboutUs } from "../features/aboutUs";
+import { ContactUs } from "../features/contactUs";
 import { Home } from "../features/home";
 import { HowWeWork } from "../features/howWeWork";
 import { Services } from "../features/services";
@@ -7,5 +8,6 @@ import { Services } from "../features/services";
     {path: "/", element: <Home />},
     {path: "/services", element: <Services />},
     {path: "/about-us", element: <AboutUs />},
-    {path: "/how-we-work", element: <HowWeWork/>}
+    {path: "/how-we-work", element: <HowWeWork/>},
+    {path: "/contact-us", element: <ContactUs />}
   ];
