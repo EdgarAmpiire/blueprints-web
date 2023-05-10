418bcdf0b15dd226ca59f37304795fdfd138fddd
edgarampiire <aedgar990@gmail.com>
2023-05-10 16:26:06 +0300

    home page - about us

---
+++ src/features/howWeWork/routes/HowWeWork.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { MainLayout } from "../../../components/layout/MainLayout";
+
+export const HowWeWork = () => {
+  return (
+    <div>
+<MainLayout page="how-we-work" >
+
+        howWeWork
+</MainLayout>
+        </div>
+      )
+}
