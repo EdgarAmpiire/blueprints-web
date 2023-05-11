118ad300a8653fff76d520131fad774a9458654c
user <user@users-iMac.local>
2023-05-11 12:56:51 +0300

    home - tab

---
+++ src/features/home/components/Strategy.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Strategy = () => {
+  return (
+    <div>
+        <div>
+            <div>
+            We work together to outline the path to meeting your vision, and define the best product ideas to achieve your business goals to reach product-market fit
+            </div>
+        </div>
+    </div>
+  )
+}
