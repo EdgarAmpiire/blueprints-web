164e09825201bd7634cf51774d030714cbf800a6
user <user@users-iMac.local>
2023-05-11 10:15:54 +0300

    feat: home - hero

--- src/features/home/components/Hero.jsx
+++ src/features/home/components/Hero.jsx
@@ -6,17 +6,17 @@ export const Hero = () => {
     <div>
       <div className="flex w-full mt-12 ">
         <div className="w-8/12">
-            <div>
-
-          <h1>
-            Delivering Solutions Powered by People, Process, and Technology
-          </h1>
-          <p>
-            We partners with businesses and product teams to conceptualize,
-            design, build and position their digital products and services to
-            scale and succeed in existing or new markets in the digital economy.
-          </p>
-            </div>
+          <div className="w-10/12">
+            <h1 className="text-4xl font-semibold tracking-wide w-10/12">
+              Delivering Solutions Powered by <span> People, Process,</span> and <span> Technology</span> 
+            </h1>
+            <p>
+              We partners with businesses and product teams to conceptualize,
+              design, build and position their digital products and services to
+              scale and succeed in existing or new markets in the digital
+              economy.
+            </p>
+          </div>
         </div>
         <div className="w-4/12">
           <img src={thumb} alt="" />
