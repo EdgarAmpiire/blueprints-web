bb4e567e842e1a132e8163d2ebd87f1c56d34d26
user <user@users-iMac.local>
2023-05-11 10:04:46 +0300

    feat: home - hero

--- src/features/home/components/Hero.jsx
+++ src/features/home/components/Hero.jsx
@@ -5,7 +5,11 @@ export const Hero = () => {
   return (
     <div>
         <div>
+            <h1>
+
         Delivering Solutions Powered by People, Process, and Technology
+            </h1>
+            <p>We partners with businesses and product teams to conceptualize, design, build and position their digital products and services  to scale and succeed in existing or new markets in the digital economy.</p>
         </div>
         <div>
             <img src={thumb} alt="" />
