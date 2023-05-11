b0269d7e66a59c70288d13b52a10233277665cce
user <user@users-iMac.local>
2023-05-11 15:19:17 +0300

    home - home blog

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -5,7 +5,7 @@ export const MainLayout = ({ page, children }) => {
   return (
     <div className="flex items-center justify-center ">
 
-    <div className="mx-20 lg:mx-32 max-w-5xl ">
+    <div className="mx-20 lg:mx-32 max-w-5xl mb-24 ">
       <Navbar page={page} />
       {children}
     </div>
---
+++ src/features/home/components/HomeBlog.jsx
@@ -0,0 +1,17 @@
+ import React from 'react'
+ 
+ export const HomeBlog = () => {
+   return (
+     <div>
+        <div className="mt-24">
+        <div className="w-full flex justify-center ">
+          <div className="px-6 py-1 bg-primary"></div>
+        </div>
+        <div className="w-full flex justify-center mt-4 ">
+          <h1 className="text-3xl font-semibold">We help you with</h1>
+        </div>
+        </div>
+     </div>
+   )
+ }
+ 
