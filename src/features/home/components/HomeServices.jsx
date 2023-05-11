5e09a5e24f798c3d9fb3ece7a45762b2bb3c356b
user <user@users-iMac.local>
2023-05-11 11:41:07 +0300

    feat: home - services

--- src/features/home/components/HomeServices.jsx
+++ src/features/home/components/HomeServices.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
+import React from "react";
 
 export const HomeServices = () => {
   return (
     <div>
-        <div className=''>
-            <div className='w-full flex justify-center '>
-    <div className='px-3 py-1 bg-primary'>
-
-    </div>
-            </div>
+      <div className="mt-24">
+        <div className="w-full flex justify-center ">
+          <div className="px-6 py-1 bg-primary"></div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
