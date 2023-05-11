86136c0d70842334da177269029ba5556c12e775
user <user@users-iMac.local>
2023-05-11 15:22:56 +0300

    home - home blog

--- src/features/home/components/HomeBlog.jsx
+++ src/features/home/components/HomeBlog.jsx
@@ -7,8 +7,9 @@
         <div className="w-full flex justify-start ">
           <div className="px-6 py-1 bg-primary"></div>
         </div>
-        <div className="w-6/12 flex justify-start mt-4 ">
-          <h1 className="text-3xl font-semibold">People, Process and Technology are always in motion.</h1>
+        <div className="w-8/12 flex flex-col justify-start mt-4 ">
+          <h1 className="text-3xl w-9/12 font-semibold">People, Process and Technology are always in motion.</h1>
+          <p className='mt-5 text-lg text-gray-500 '>Ultimately, these three interdependent elements are a key strategic component for digital operations to reach consistent efficiency, automated scaling, and achieve goals and outcomes</p>
         </div>
         </div>
      </div>
