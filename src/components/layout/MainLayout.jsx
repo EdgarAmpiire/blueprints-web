bfb46fb8db2f944c939e3deea8d32120f82f32e2
user <user@users-iMac.local>
2023-05-11 10:33:32 +0300

    feat: home - hero

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -3,9 +3,12 @@ import { Navbar } from "./components/Navbar";
 
 export const MainLayout = ({ page, children }) => {
   return (
-    <div className="mx-20">
+    <div className="flex items-center justify-center ">
+
+    <div className="mx-20 lg:mx-32 max-w-5xl ">
       <Navbar page={page} />
       {children}
+    </div>
     </div>
   );
 };
