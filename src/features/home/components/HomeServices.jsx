60c0af887b8758963fce4dd3c9612f21e2d4446b
user <user@users-iMac.local>
2023-05-11 12:49:26 +0300

    home - tab

--- src/features/home/components/HomeServices.jsx
+++ src/features/home/components/HomeServices.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 export const HomeServices = () => {
+    const [isActive, setisActive] = React.useState("history");
+
+  const changeComponent = (item) => {
+    setisActive(item);
+  };
   return (
     <div>
       <div className="mt-24">
@@ -13,7 +18,58 @@ export const HomeServices = () => {
 
         <div className="bg-[#F5F5F5] py-5 px-3 rounded-lg mt-5">
           
-
+        <div className="my-12">
+            <div className=" border-gray-300">
+              <div className="flex justify-around w-full ">
+                <button
+                  className={
+                    isActive === "strategy"
+                      ? "mx-1 bg-black text-white rounded-full py-1.5 text-sm px-3"
+                      : "mx-1 bg-[#E5E5E5] rounded-full py-1.5 text-sm text-black px-3"
+                  }
+                  onClick={() => changeComponent("strategy")}
+                >
+                  Business & Product Strategy
+                </button>
+                <button
+                  className={
+                    isActive === "management"
+                      ? "mx-1 bg-black text-white rounded-full py-1.5 text-sm px-3"
+                      : "mx-1 bg-[#E5E5E5] rounded-full py-1.5 text-sm text-black px-3"
+                  }
+                  onClick={() => changeComponent("management")}
+                >
+                  Product Portfolio Management
+                </button>
+                <button
+                  className={
+                    isActive === "operation"
+                      ? "mx-1 bg-black text-white rounded-full py-1.5 text-sm px-3"
+                      : "mx-1 bg-[#E5E5E5] rounded-full py-1.5 text-sm text-black px-3"
+                  }
+                  onClick={() => changeComponent("operation")}
+                >
+                  Product Development and Management Operation 
+                </button>
+                <button
+                  className={
+                    isActive === "marketing"
+                      ? "mx-1 bg-black text-white rounded-full py-1.5 text-sm px-3"
+                      : "mx-1 bg-[#E5E5E5] rounded-full py-1.5 text-sm text-black px-3"
+                  }
+                  onClick={() => changeComponent("marketing")}
+                >
+                  Digital Marketing
+                </button>
+              </div>
+            </div>
+            <div>
+              {isActive === "strategy" && "tab1"}
+              {isActive === "management" && "tab2"}
+              {isActive === "operation" && "tab3"}
+              {isActive === "marketing" && "tab4"}
+            </div>
+          </div>
           
         </div>
       </div>
