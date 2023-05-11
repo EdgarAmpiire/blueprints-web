b0c95961d06ec3473909b37260a944948b32900a
user <user@users-iMac.local>
2023-05-11 13:13:58 +0300

    home - tab

--- src/features/home/components/Strategy.jsx
+++ src/features/home/components/Strategy.jsx
@@ -5,27 +5,27 @@ import { Link } from "react-router-dom";
 export const Strategy = () => {
   return (
     <div>
-      <div className="w-full">
-        <div className="flex  ">
-            <div>
-
-          <div className="mt-5 w-7/12 flex justify-center items-center flex-col ">
-
-            <h1 className="text-gray-500 text-lg w-10/12 ">
-
-            We work together to outline the path to meeting your vision, and
-            define the best product ideas to achieve your business goals to
-            reach product-market fit
-            </h1>
+      <div className="w-full flex justify-center ">
+        <div className="flex w-10/12 ">
+          <div className="w-8/12">
+            <div className="mt-5  flex justify-center  flex-col ">
+              <h1 className="text-gray-500 text-lg w-10/12 ">
+                We work together to outline the path to meeting your vision, and
+                define the best product ideas to achieve your business goals to
+                reach product-market fit
+              </h1>
             </div>
 
-            <Link className="text-primary">Inquire more about this  </Link>
+            <div className="mt-8">
+              <Link to={"/"} className="text-primary">
+                Inquire more about this{" "}
+              </Link>
+            </div>
           </div>
-          <div className="w-5/12 ">
-
-          <div className="flex justify-center items-center">
-            <img src={thumb} alt="" />
-          </div>
+          <div className="w-4/12 ">
+            <div className="flex w-full justify-center items-center">
+              <img src={thumb} alt="" />
+            </div>
           </div>
         </div>
       </div>
