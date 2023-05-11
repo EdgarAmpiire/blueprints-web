b77b618b580c168d24ec2264a01e76f8cc05296d
user <user@users-iMac.local>
2023-05-11 10:01:12 +0300

    feat: home - hero

---
+++ src/features/home/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <div>
+        <div>
+        Delivering Solutions Powered by People, Process, and Technology
+        </div>
+        <div>
+            <img src="" alt="" />
+        </div>
+    </div>
+  )
+}
