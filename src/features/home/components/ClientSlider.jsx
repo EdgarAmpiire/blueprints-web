45fdb27996fe81afe82981313f309b009e3230f5
user <user@users-iMac.local>
2023-05-11 14:59:08 +0300

    home - clients

--- src/features/home/components/ClientSlider.jsx
+++ src/features/home/components/ClientSlider.jsx
@@ -1,66 +1,77 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import logitech from "../../../assets/logitech.png"
-import h from "../../../assets/h.png"
-import army from "../../../assets/army.png"
-import eightyFour from "../../../assets/84.png"
-import gid from "../../../assets/gid.png"
-import blueCross from "../../../assets/blue-cross.png"
-import nike from "../../../assets/nike.png"
-import ingram from "../../../assets/ingram.png"
-import cnb from "../../../assets/cnb.png"
-import honda from "../../../assets/honda.png"
-import avery from "../../../assets/avery.png"
-import corelogic from "../../../assets/corelogic.png"
-import deloitte from "../../../assets/deloitte.png"
-import farmers from "../../../assets/farmers.png"
-import ibm from "../../../assets/ibm.png"
-import zelle from "../../../assets/zelle.png"
-import lewray from "../../../assets/lewray.png"
-import adobe from "../../../assets/adobe.png"
+import logitech from "../../../assets/logitech.png";
+import h from "../../../assets/h.png";
+import army from "../../../assets/army.png";
+import eightyFour from "../../../assets/84.png";
+import gid from "../../../assets/gid.png";
+import blueCross from "../../../assets/blue-cross.png";
+import nike from "../../../assets/nike.png";
+import ingram from "../../../assets/ingram.png";
+import cnb from "../../../assets/cnb.png";
+import honda from "../../../assets/honda.png";
+import avery from "../../../assets/avery.png";
+import corelogic from "../../../assets/corelogic.png";
+import deloitte from "../../../assets/deloitte.png";
+import farmers from "../../../assets/farmers.png";
+import ibm from "../../../assets/ibm.png";
+import zelle from "../../../assets/zelle.png";
+import lewray from "../../../assets/lewray.png";
+import adobe from "../../../assets/adobe.png";
 
 class ClientSlider extends Component {
-    render(){
-        const settings = {
-            dots: false,
-            infinite: true,
-      slidesToShow: 3,
+  render() {
+    const settings = {
+      dots: false,
+      infinite: true,
+      slidesToShow: 5,
       slidesToScroll: 1,
-      autoplay: true,
-      speed: 1000,
+    //   autoplay: true,
+      speed: 2000,
       autoplaySpeed: 2000,
-      cssEase: "linear"
-    }
-        
-        return(
-            <div>
-                <Slider {...settings}>
-                     <div>
-                        <img src={logitech} alt="" />
-                     </div>
-                     <div>
-                        <img src={h} alt="" />
-                     </div><div>
-                        <img src={army} alt="" />
-                     </div><div>
-                        <img src={eightyFour} alt="" />
-                     </div><div>
-                        <img src={gid} alt="" />
-                     </div><div>
-                        <img src={blueCross} alt="" />
-                     </div><div>
-                        <img src={nike} alt="" />
-                     </div><div>
-                        <img src={ingram} alt="" />
-                     </div><div>
-                        <img src={cnb} alt="" />
-                     </div>
-                </Slider>
+      cssEase: "linear",
+    };
+
+    return (
+      <div className="mt-10 ">
+        <Slider {...settings}>
+            
+          <div className=" h-24">
+            <div className="flex items-center justify-center h-full ">
+
+            <img src={logitech} alt="" className=""/>
             </div>
-        )
-    }
+          </div>
+          <div className="flex items-center justify-center w-full">
+            <img src={h} alt="" />
+          </div>
+          <div className="flex items-center justify-center w-full">
+            <img src={army} alt="" />
+          </div>
+          <div className="flex items-center justify-center w-full">
+            <img src={eightyFour} alt="" />
+          </div>
+          <div className="flex items-center justify-center w-full">
+            <img src={gid} alt="" />
+          </div>
+          <div className="flex items-center justify-center w-full">
+            <img src={blueCross} alt="" />
+          </div>
+          <div className="flex items-center justify-center w-full">
+            <img src={nike} alt="" />
+          </div>
+          <div className="flex items-center justify-center w-full">
+            <img src={ingram} alt="" />
+          </div>
+          <div className="flex items-center justify-center w-full">
+            <img src={cnb} alt="" />
+          </div>
+        </Slider>
+      </div>
+    );
+  }
 }
 
 export default ClientSlider;
