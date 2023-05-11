a5aa4eb37dbba308bd3c5f0318ba0b79c1b5d61b
user <user@users-iMac.local>
2023-05-11 10:07:55 +0300

    feat: home - hero

--- src/features/home/components/Hero.jsx
+++ src/features/home/components/Hero.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import thumb from "../../../assets/thumb.png"
+import React from "react";
+import thumb from "../../../assets/thumb.png";
 
 export const Hero = () => {
   return (
     <div>
-        <div>
-            <h1>
+      <div className="flex w-full mt-12 ">
+        <div className="w-8/12">
+            <div>
 
-        Delivering Solutions Powered by People, Process, and Technology
-            </h1>
-            <p>We partners with businesses and product teams to conceptualize, design, build and position their digital products and services  to scale and succeed in existing or new markets in the digital economy.</p>
+          <h1>
+            Delivering Solutions Powered by People, Process, and Technology
+          </h1>
+          <p>
+            We partners with businesses and product teams to conceptualize,
+            design, build and position their digital products and services to
+            scale and succeed in existing or new markets in the digital economy.
+          </p>
+            </div>
         </div>
-        <div>
-            <img src={thumb} alt="" />
+        <div className="w-4/12">
+          <img src={thumb} alt="" />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
