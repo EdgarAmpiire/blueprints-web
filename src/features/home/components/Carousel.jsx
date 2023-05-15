5efbde8e9b157a6dbeab49526cd2e266e0071e75
user <user@users-iMac.local>
2023-05-15 15:14:45 +0300

    home - carousel

--- src/features/home/components/Carousel.jsx
+++ src/features/home/components/Carousel.jsx
@@ -54,7 +54,7 @@ export const Carousel = () => {
 
   return (
     <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative' >
-      <div style={{backgroundImage: `url(${slides[1].url})`}} className='w-full h-full rounded-2xl bg-center bg-cover duration-500 '> </div>
+      <div style={{backgroundImage: `url(${slides[0].url})`}} className='w-full h-full rounded-2xl bg-center bg-cover duration-500 '> </div>
     </div>
   )
 }
