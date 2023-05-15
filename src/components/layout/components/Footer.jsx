1badc7e1132f5857f62e24f6d08babd71712d78d
user <user@users-iMac.local>
2023-05-15 18:02:07 +0300

    Footer

--- src/components/layout/components/Footer.jsx
+++ src/components/layout/components/Footer.jsx
@@ -3,26 +3,34 @@ import React from "react";
 export const Footer = () => {
   return (
     <div className="">
-      <div className="mt-20 bg-black py-6 px-5">
-       
-       <div>
-        <h1 className="text-white">HEY....WAIT!</h1>
-        <p className="text-white">Subscribe to our newsletters </p>
-       </div>
-       <div>
+      <div className="mt-20 bg-black py-6 px-5 w-full">
+        <h1 className="text-white text-xl">HEY....WAIT!</h1>
 
-<form>
-    <div className="flex">
-        <label for="search-dropdown" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
-        
-        <div className="relative w-full">
-            <input type="search" id="search-dropdown" className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50   border border-gray-300  focus:outline-none " placeholder="Your email address" required />
-            <button type="submit" className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-[#0055BA]  border border-[#0055BA]  focus:outline-none ">SUBSCRIBE</button>
+        <div className="flex w-full items-center justify-between mt-4">
+          <div className="w-5/12">
+            <p className="text-white">Subscribe to our newsletters </p>
+          </div>
+          <div className="w-7/12">
+            <form>
+              <div className="flex">
+                <div className="relative w-full">
+                  <input
+                    type="search"
+                    id="search-dropdown"
+                    className="block p-2.5 w-full z-20 text-sm text-white bg-[#E5E5E533] opacity-100   border border-[#E5E5E533] focus:outline-none "
+                    placeholder="Your email address"
+                    required
+                  />
+                  <button
+                    type="submit"
+                    className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-[#0055BA]  border border-[#0055BA]  focus:outline-none ">
+                    SUBSCRIBE
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
-    </div>
-</form>
-
-       </div>
       </div>
     </div>
   );
