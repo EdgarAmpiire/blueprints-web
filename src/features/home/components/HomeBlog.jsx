8a2fde3e79274ed96005a11da5947a3e5ea92910
user <user@users-iMac.local>
2023-05-15 16:49:57 +0300

    home - carousel

--- src/features/home/components/HomeBlog.jsx
+++ src/features/home/components/HomeBlog.jsx
@@ -11,7 +11,7 @@ import { Carousel } from './Carousel'
           <div className="px-6 py-1 bg-primary"></div>
         </div>
         <div className="w-8/12 flex flex-col justify-start mt-4 ">
-          <h1 className="text-3xl w-9/12 font-semibold">People, Process and Technology are always in motion.</h1>
+          <h1 className="text-3xl text-[#040052] w-9/12 font-semibold">People, Process and Technology are always in motion.</h1>
           <p className='mt-5 text-lg text-gray-500 '>Ultimately, these three interdependent elements are a key strategic component for digital operations to reach consistent efficiency, automated scaling, and achieve goals and outcomes</p>
         </div>
         </div>
