963e4fe6ca3041a2dbb1d1453674288087d512cb
user <user@users-iMac.local>
2023-05-15 17:24:41 +0300

    Footer

--- src/components/layout/components/Footer.jsx
+++ src/components/layout/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <div className=''>
-        <div className='mt-20 bg-black py-5'>
+        <div className='mt-20 bg-black py-6 px-5'>
 <h1 className='text-white'>footer</h1>
         </div>
     </div>
