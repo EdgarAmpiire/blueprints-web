b6b6e95f59e851203608ecab30d1729f82ea7b34
edgarampiire <aedgar990@gmail.com>
2023-05-16 15:23:04 +0300

    services - hero

--- src/features/services/components/Hero.jsx
+++ src/features/services/components/Hero.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export const Hero = () => {
   return (
     <div>
-      <div>
-        <h1>Let’s build a wining blueprint</h1>
-        <p>
+      <div className="mt-14 flex flex-col items-center justify-center w-full">
+    <h1 className="text-4xl font-semibold text-[#040052]">Let’s build a wining blueprint</h1>
+        <p className="w-10/12 text-center mt-8 font-light">
            We are much more than consultants - as digital allies,  we guide you
           in preparing for present and future opportunities by bridging strategy
           to execution, planning and taking action that accelerates growth and
