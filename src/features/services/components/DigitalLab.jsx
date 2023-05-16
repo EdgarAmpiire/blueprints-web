68de707029d561e4f78a1de67bb6114e150a5804
edgarampiire <aedgar990@gmail.com>
2023-05-16 17:06:36 +0300

    services - digital lab

--- src/features/services/components/DigitalLab.jsx
+++ src/features/services/components/DigitalLab.jsx
@@ -18,8 +18,12 @@ export const DigitalLab = () => {
             </div>
             <div className="flex flex-col items-start ">
               <div>
-                <h1 className="text-[#040052] text-lg font-semibold">Business and Product Strategy</h1>
-                <h3 className="mt-5 text-[#040052]">Investing in a plan for Growth, Revenue, and Profits</h3>
+                <h1 className="text-[#040052] text-lg font-semibold">
+                  Business and Product Strategy
+                </h1>
+                <h3 className="mt-5 text-[#040052]">
+                  Investing in a plan for Growth, Revenue, and Profits
+                </h3>
               </div>
               <div className="mt-5 w-8/12">
                 <p className="text-[#6B6A6A] text-sm">
@@ -39,15 +43,18 @@ export const DigitalLab = () => {
 
         <div className="bg-[#F5E5FF] rounded-xl py-10 px-10 mt-16">
           <div className="grid grid-cols-2 w-full">
-            
             <div className="flex flex-col items-start ">
               <div>
-                <h1 className="text-[#040052] text-lg font-semibold">Digital Marketing</h1>
+                <h1 className="text-[#040052] text-lg font-semibold">
+                  Digital Marketing
+                </h1>
                 <h3 className="mt-5 text-[#040052]">Connect and Convert</h3>
               </div>
               <div className="mt-5 w-8/12">
                 <p className="text-[#6B6A6A] text-sm">
-                A digital marketing strategy is a set of planned, strategic and tactical actions to acquire and retain the attention of your addressable market
+                  A digital marketing strategy is a set of planned, strategic
+                  and tactical actions to acquire and retain the attention of
+                  your addressable market
                 </p>
                 <p className="mt-5 text-[#6B6A6A] text-sm ">Services:</p>
                 <ul class="list-disc mt-5 text-[#6B6A6A] text-sm ml-8">
@@ -69,22 +76,55 @@ export const DigitalLab = () => {
             </div>
             <div className="flex flex-col items-start ">
               <div>
-                <h1 className="text-[#040052] text-lg font-semibold">Product Development and Product Management Operations (ProdOps)</h1>
-                <h3 className="mt-5 text-[#040052]">From Concept to Commercialization</h3>
+                <h1 className="text-[#040052] text-lg font-semibold">
+                  Product Development and Product Management Operations
+                  (ProdOps)
+                </h1>
+                <h3 className="mt-5 text-[#040052]">
+                  From Concept to Commercialization
+                </h3>
               </div>
               <div className="mt-5 w-8/12">
                 <p className="text-[#6B6A6A] text-sm">
-                Product development is the end-to-end process of driving an idea from concept to commercialization and beyond in its lifecycle. 
+                  Product development is the end-to-end process of driving an
+                  idea from concept to commercialization and beyond in its
+                  lifecycle.
                 </p>
                 <p className="mt-5 text-[#6B6A6A] text-sm ">Services:</p>
                 <ul class="list-disc mt-5 text-[#6B6A6A] text-sm ml-8">
-                  <li>Product Development  </li>
+                  <li>Product Development </li>
                   <li>Product Operations (ProdOps) </li>
-                  <li>Design Thinking and Innovation  </li>
+                  <li>Design Thinking and Innovation </li>
                   <li>User experience (UX) design </li>
-
                 </ul>
               </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-[#F5E5FF] rounded-xl py-10 px-10 mt-16">
+          <div className="grid grid-cols-2 w-full">
+            <div className="flex flex-col items-start ">
+              <div>
+                <h1 className="text-[#040052] text-lg font-semibold">
+                  Product Portfolio Management
+                </h1>
+                <h3 className="mt-5 text-[#040052]">Investing in value</h3>
+              </div>
+              <div className="mt-5 w-8/12">
+                <p className="text-[#6B6A6A] text-sm">
+                Leverage BSG’s business and product strategy team to kickstart product thinking at your organization.
+                </p>
+                <p className="mt-5 text-[#6B6A6A] text-sm ">Services:</p>
+                <ul class="list-disc mt-5 text-[#6B6A6A] text-sm ml-8">
+                  <li>Small Business Digital Consulting </li>
+                  <li>Business Strategy Builder </li>
+                  <li>Product Strategy Builder </li>
+                </ul>
+              </div>
+            </div>
+            <div className="flex items-center justify-center ">
+              <img src={thumb} alt="" />
             </div>
           </div>
         </div>
