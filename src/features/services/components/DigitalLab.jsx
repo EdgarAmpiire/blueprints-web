4264d70c678cb74212038b67348e62e79deb90f4
edgarampiire <aedgar990@gmail.com>
2023-05-16 16:25:15 +0300

    services - digital lab

---
+++ src/features/services/components/DigitalLab.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const DigitalLab = () => {
+  return(
+    <div>
+        <div>
+            <div>
+                <h1>Our Digital Lab </h1>
+            </div>
+        </div>
+    </div>
+    )
+};
