b609ec152143789f23a97681c279105087da521a
edgarampiire <aedgar990@gmail.com>
2023-05-16 11:50:24 +0300

    home - clinets

--- src/features/home/components/Clients.jsx
+++ src/features/home/components/Clients.jsx
@@ -5,10 +5,10 @@ export const Clients = () => {
   return (
     <div>
       <div className="mt-24">
-        <div className="w-full flex justify-center ">
+        <div className="w-full flex justify-start ">
           <div className="px-6 py-1 bg-primary"></div>
         </div>
-        <div className="w-full flex justify-center mt-4 ">
+        <div className="w-full flex justify-start mt-4 ">
           <h1 className="text-lg text-gray-500">
             Trusted by select clients, companies & partners
           </h1>
