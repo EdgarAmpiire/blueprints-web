d73712d645d383e9bffce4237281613ddcba674a
edgarampiire <aedgar990@gmail.com>
2023-05-16 15:18:12 +0300

    services - hero

---
+++ src/features/services/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <div>
+      <div>
+        <h1>Let’s build a wining blueprint</h1>
+        <p>
+           We are much more than consultants - as digital allies,  we guide you
+          in preparing for present and future opportunities by bridging strategy
+          to execution, planning and taking action that accelerates growth and
+          delivers success on all levels
+        </p>
+      </div>
+    </div>
+  );
+};
--- src/features/services/routes/Services.jsx
+++ src/features/services/routes/Services.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import {MainLayout} from "../../../components/layout/MainLayout"
+import { Hero } from "../components/Hero";
 
 export const Services = () => {
   return (
     <div>
     <MainLayout page="services">
-       Services
+       <div>
+        <Hero />
+       </div>
       </MainLayout>
     </div>
   );
