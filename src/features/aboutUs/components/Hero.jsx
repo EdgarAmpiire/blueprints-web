346e2fa5dd7b72334a724d9a43e6569a3b94bb22
user <user@users-iMac.local>
2023-05-17 12:32:48 +0300

    footer

--- src/features/aboutUs/components/Hero.jsx
+++ src/features/aboutUs/components/Hero.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
-// import bg from "../../../assets/bg.png"
 
 export const Hero = () => {
   return (
-    <div className="container mx-auto">
-			<div className="md:min-h-screen">
-				<div
-					className="hero h-100 bg-container bg-center bg-blend-overlay bg-primaryColor"
-					style={{ backgroundImage: `url(/images/bg.png)` }}>
-					<div className="heading-container absolute flex flex-col justify-center items-center w-full">
-						<h1 className="font-bold text-3xl text-black ">How can we help?</h1>
-						<p className="text-lg text-black mt-5">
-							Use specific terms for the most relevant results
-						</p> 
-						
-					</div>
-				</div>
-</div>
-</div>
+    <div>
+        <div class="w-full bg-cover bg-center" style="height:32rem; background-image: url(https://images.unsplash.com/photo-1504384308090-c894fdcc538d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80);">
+        <div class="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50">
+            <div class="text-center">
+                <h1 class="text-white text-2xl font-semibold uppercase md:text-3xl">Build Your new <span class="underline text-blue-400">Saas</span></h1>
+                <button class="mt-4 px-4 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Start project</button>
+            </div>
+        </div>
+    </div>
+    </div>
   )
 }
