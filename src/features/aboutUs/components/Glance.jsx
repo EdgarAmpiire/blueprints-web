269899d115a33db7e30df21002d4117246ba33be
edgarampiire <aedgar990@gmail.com>
2023-05-18 13:39:42 +0300

    about us - hero

--- src/features/aboutUs/components/Glance.jsx
+++ src/features/aboutUs/components/Glance.jsx
@@ -10,14 +10,14 @@ export const Glance = () => {
         <div className="flex items-center justify-center">
             <h1 className="text-[#040052] font-semibold text-xl">At a glance</h1>
         </div>
-        <div className="flex justify-between items-center w-7/12  gap-10 mt-10">
+        <div className="flex justify-between items-center w-8/12  gap-10 mt-10">
           <div className="flex items-center justify-center flex-col w-4/12">
             <div className=" py-2 px-2 rounded-full flex items-center justify-center h-[5.5rem] w-[5.5rem] ">
               <img src={vision} alt="" className="w-fit" />
             </div>
             <div className="mt-2 flex flex-col items-center justify-center ">
               <p className="text-[#040052] font-semibold uppercase">Our Vision</p>
-              <p className="text-xs text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+              <p className="text-xs text-center mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
             </div>
           </div>
 
@@ -27,7 +27,7 @@ export const Glance = () => {
             </div>
             <div className="mt-2 flex flex-col items-center justify-center ">
               <p className="text-[#040052] font-semibold uppercase">Our Culture</p>
-              <p className="text-xs text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+              <p className="text-xs text-center mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
 
             </div>
           </div>
@@ -38,7 +38,7 @@ export const Glance = () => {
             </div>
             <div className="mt-2 flex flex-col items-center justify-center ">
               <p className="text-[#040052] font-semibold uppercase">Our Team</p>
-              <p className="text-xs text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+              <p className="text-xs text-center mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
 
             </div>
           </div>
