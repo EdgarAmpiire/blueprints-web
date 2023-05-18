0304363c5038dafadc48868ce81a123ce204bad3
edgarampiire <aedgar990@gmail.com>
2023-05-18 12:17:54 +0300

    about us - hero

---
+++ src/features/aboutUs/components/Glance.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Glance = () => {
+  return (
+  <div>
+    <div>
+        <div>
+            <h1>At a glance</h1>
+        </div>
+    </div>
+  </div>
+    )
+};
