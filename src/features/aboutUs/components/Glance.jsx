c7f17e04d4aa7150badabf08120fb2afed811b2d
edgarampiire <aedgar990@gmail.com>
2023-05-18 12:56:14 +0300

    about us - hero

--- src/features/aboutUs/components/Glance.jsx
+++ src/features/aboutUs/components/Glance.jsx
@@ -6,17 +6,17 @@ import team from "../../../assets/team.png"
 export const Glance = () => {
   return (
   <div>
-    <div className="mt-14">
+    <div className="mt-14 flex items-center flex-col">
         <div className="flex items-center justify-center">
             <h1 className="text-[#040052] font-semibold text-xl">At a glance</h1>
         </div>
-        <div className="flex justify-between w-7/12 gap-10 mt-10">
+        <div className="flex justify-between items-center w-7/12  gap-10 mt-10">
           <div className="flex items-center justify-center flex-col w-4/12">
             <div className="bg-[#5A50FE] py-5 px-5 rounded-full flex items-center justify-center h-[4.5rem] w-[4.5rem] ">
               <img src={vision} alt="" className="w-fit" />
             </div>
             <div className="mt-2">
-              <p>Start-ups</p>
+              <p>Our Vision</p>
             </div>
           </div>
 
@@ -25,7 +25,7 @@ export const Glance = () => {
               <img src={culture} alt="" className="w-fit" />
             </div>
             <div className="mt-2">
-              <p>Mid Sized Enterprises</p>
+              <p>Our Culture</p>
             </div>
           </div>
 
@@ -34,7 +34,7 @@ export const Glance = () => {
               <img src={team} alt="" className="w-fit" />
             </div>
             <div className="mt-2">
-              <p>Large Corporations</p>
+              <p>Our Team</p>
             </div>
           </div>
         </div>
