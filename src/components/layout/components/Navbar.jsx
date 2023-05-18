09535150fe0b62af448b3554675f7598488a38d2
edgarampiire <aedgar990@gmail.com>
2023-05-18 11:35:32 +0300

    about us - hero

--- src/components/layout/components/Navbar.jsx
+++ src/components/layout/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
   
   
     return (
-      <nav className="relative flex flex-wrap items-center justify-between px-0 py-3 ">
+      <nav className="relative flex flex-wrap items-center justify-between px-0 py-0 ">
         <div className="container px-0 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <a
--- src/features/aboutUs/components/Hero.jsx
+++ src/features/aboutUs/components/Hero.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export const Hero = () => {
   return (
     <di className="">
-        <div class="w-full bg-cover bg-center object-cover" style={{height: "32rem", backgroundImage: `url(/images/bg.png)`}}>
-        <div class="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50">
-            <div class="text-center">
-                <h1 class="text-white text-2xl font-semibold uppercase md:text-3xl">Build Your new <span class="underline text-blue-400">Saas</span></h1>
-                <button class="mt-4 px-4 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Start project</button>
-            </div>
+      <div
+        class="w-full bg-cover bg-center object-cover"
+        style={{ height: "32rem", backgroundImage: `url(/images/bg.png)` }}>
+        <div class="flex justify-center h-full w-full bg-[#000000D9] opacity-70">
+          <div class="text-center mt-10">
+            <h1 class="text-white text-2xl font-semibold uppercase md:text-3xl">
+              We Are
+            </h1>
+            <h2 className="text-white text-xl font-semibold uppercase mt-10 ">WE ARE BLUEPRINT STRATEGY GROUP, INC</h2>
+            
+            <p></p>
+            <button class="mt-4 px-4 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+              Start project
+            </button>
+          </div>
         </div>
-    </div>
+      </div>
     </di>
-  )
-}
+  );
+};
