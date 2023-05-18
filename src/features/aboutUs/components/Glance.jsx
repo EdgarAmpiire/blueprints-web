374f0058a1a28a2156e1a3a6d138ecf415acb130
edgarampiire <aedgar990@gmail.com>
2023-05-18 13:39:04 +0300

    about us - hero

--- src/features/aboutUs/components/Glance.jsx
+++ src/features/aboutUs/components/Glance.jsx
@@ -12,29 +12,34 @@ export const Glance = () => {
         </div>
         <div className="flex justify-between items-center w-7/12  gap-10 mt-10">
           <div className="flex items-center justify-center flex-col w-4/12">
-            <div className="bg-[#5A50FE] py-5 px-5 rounded-full flex items-center justify-center h-[4.5rem] w-[4.5rem] ">
+            <div className=" py-2 px-2 rounded-full flex items-center justify-center h-[5.5rem] w-[5.5rem] ">
               <img src={vision} alt="" className="w-fit" />
             </div>
-            <div className="mt-2">
-              <p>Our Vision</p>
+            <div className="mt-2 flex flex-col items-center justify-center ">
+              <p className="text-[#040052] font-semibold uppercase">Our Vision</p>
+              <p className="text-xs text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
             </div>
           </div>
 
           <div className="flex items-center justify-center flex-col w-4/12">
-            <div className="bg-[#5A50FE] py-5 px-5 rounded-full flex items-center justify-center h-[4.5rem] w-[4.5rem] ">
+            <div className=" py-5 px-5 rounded-full flex items-center justify-center h-[5.5rem] w-[5.5rem] ">
               <img src={culture} alt="" className="w-fit" />
             </div>
-            <div className="mt-2">
-              <p>Our Culture</p>
+            <div className="mt-2 flex flex-col items-center justify-center ">
+              <p className="text-[#040052] font-semibold uppercase">Our Culture</p>
+              <p className="text-xs text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+
             </div>
           </div>
 
           <div className="flex items-center justify-center flex-col w-4/12">
-            <div className="bg-[#5A50FE] py-5 px-5 rounded-full flex items-center justify-center h-[4.5rem] w-[4.5rem] ">
+            <div className=" py-5 px-5 rounded-full flex items-center justify-center h-[5.5rem] w-[5.5rem] ">
               <img src={team} alt="" className="w-fit" />
             </div>
-            <div className="mt-2">
-              <p>Our Team</p>
+            <div className="mt-2 flex flex-col items-center justify-center ">
+              <p className="text-[#040052] font-semibold uppercase">Our Team</p>
+              <p className="text-xs text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+
             </div>
           </div>
         </div>
