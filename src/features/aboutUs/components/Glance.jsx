5d2f879ff2e151a6e8204594fd3dda89aa36d76c
edgarampiire <aedgar990@gmail.com>
2023-05-18 12:25:45 +0300

    about us - hero

--- src/features/aboutUs/components/Glance.jsx
+++ src/features/aboutUs/components/Glance.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export const Glance = () => {
   return (
   <div>
-    <div>
-        <div>
-            <h1>At a glance</h1>
+    <div className="mt-14">
+        <div className="flex items-center justify-center">
+            <h1 className="text-[#040052] font-semibold text-xl">At a glance</h1>
         </div>
     </div>
   </div>
