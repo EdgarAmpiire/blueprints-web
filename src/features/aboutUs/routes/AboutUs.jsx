5c0b704f8d4aba4e747285a39292bf94d7f204e0
edgarampiire <aedgar990@gmail.com>
2023-05-18 12:24:14 +0300

    about us - hero

--- src/features/aboutUs/routes/AboutUs.jsx
+++ src/features/aboutUs/routes/AboutUs.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { MainLayout } from "../../../components/layout/MainLayout";
 import { Hero } from "../components/Hero";
+import { Glance } from "../components/Glance";
 
 export const AboutUs = () => {
   return (
     <div>
       <MainLayout page="about-us">
         <Hero />
+        <Glance />
       </MainLayout>
     </div>
   );
