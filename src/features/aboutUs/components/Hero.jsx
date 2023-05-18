22edac9fec35f2f995ecf46d6f04d5ebe825f2eb
edgarampiire <aedgar990@gmail.com>
2023-05-18 12:01:18 +0300

    about us - hero

--- src/features/aboutUs/components/Hero.jsx
+++ src/features/aboutUs/components/Hero.jsx
@@ -6,17 +6,23 @@ export const Hero = () => {
       <div
         class="w-full bg-cover bg-center object-cover"
         style={{ height: "32rem", backgroundImage: `url(/images/bg.png)` }}>
-        <div class="flex justify-center h-full w-full bg-[#000000D9] opacity-70">
-          <div class="text-center mt-10">
+        <div class="flex flex-col items-center justify-between h-full w-full bg-[#000000D9] opacity-70">
+          <div class="text-center flex flex-col items-center  mt-10">
             <h1 class="text-white text-2xl font-semibold uppercase md:text-3xl">
               We Are
             </h1>
-            <h2 className="text-white text-xl font-semibold uppercase mt-10 ">WE ARE BLUEPRINT STRATEGY GROUP, INC</h2>
-            
-            <p></p>
-            <button class="mt-4 px-4 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
-              Start project
-            </button>
+            <h2 className="text-white text-xl font-semibold uppercase mt-10 ">
+              WE ARE BLUEPRINT STRATEGY GROUP, INC
+            </h2>
+
+            <p className="text-white mt-10 w-6/12 font-light">
+               We are  a digital consulting firm that partners with businesses
+              to conceptualize, design, build and position their digital
+              products and services
+            </p>
+          </div>
+          <div>
+            <button className="bg-white py-2 px-4 rounded-md text-black text-sm">Want to know how we work </button>
           </div>
         </div>
       </div>
