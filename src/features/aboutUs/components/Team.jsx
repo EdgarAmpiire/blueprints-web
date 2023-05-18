d813181f930ffd30001fef35edb11294225281d5
edgarampiire <aedgar990@gmail.com>
2023-05-18 14:35:39 +0300

    about us - team section

--- src/features/aboutUs/components/Team.jsx
+++ src/features/aboutUs/components/Team.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import user from "../../../assets/user.png"
+import user from "../../../assets/user.png";
+import { CiLinkedin, CiTwitter } from "react-icons/ci";
+import { AiOutlineMail } from "react-icons/ai";
 
 export const Team = () => {
   return (
@@ -10,12 +12,19 @@ export const Team = () => {
             The Team that makes it happen
           </h1>
         </div>
-        <div className="w-10/12 ">
-          <div>
-            <div>
-              <img src={user} alt="" />
+        <div className=" mt-14 flex items-center justify-center ">
+          <div className="flex w-10/12">
+            <div className="w-2/12 flex flex-col">
+              <div>
+                <img src={user} alt="" />
+              </div>
+              <div className="flex">
+                <CiLinkedin />
+                <CiTwitter />
+                <AiOutlineMail />
+              </div>
             </div>
-            <div>
+            <div className="w-10/12">
               <div>
                 <h1>E Michael Rosales.MBA, CSPO, CPIP, CSM</h1>
                 <p>Chief Digital Product Consultant, BSG Co-founder</p>
