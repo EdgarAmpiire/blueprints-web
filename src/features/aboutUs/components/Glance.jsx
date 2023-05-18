b4b6582350cce81d5093b8ca7e797422f14e660d
edgarampiire <aedgar990@gmail.com>
2023-05-18 12:49:50 +0300

    about us - hero

--- src/features/aboutUs/components/Glance.jsx
+++ src/features/aboutUs/components/Glance.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import vision from "../../../assets/vision.png"
+import culture from "../../../assets/culture.png"
+import team from "../../../assets/team.png"
 
 export const Glance = () => {
   return (
@@ -6,6 +9,34 @@ export const Glance = () => {
     <div className="mt-14">
         <div className="flex items-center justify-center">
             <h1 className="text-[#040052] font-semibold text-xl">At a glance</h1>
+        </div>
+        <div className="flex justify-between w-7/12 gap-10 mt-10">
+          <div className="flex items-center justify-center flex-col w-4/12">
+            <div className="bg-[#5A50FE] py-5 px-5 rounded-full flex items-center justify-center h-[4.5rem] w-[4.5rem] ">
+              <img src={vision} alt="" className="w-fit" />
+            </div>
+            <div className="mt-2">
+              <p>Start-ups</p>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-center flex-col w-4/12">
+            <div className="bg-[#5A50FE] py-5 px-5 rounded-full flex items-center justify-center h-[4.5rem] w-[4.5rem] ">
+              <img src={culture} alt="" className="w-fit" />
+            </div>
+            <div className="mt-2">
+              <p>Mid Sized Enterprises</p>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-center flex-col w-4/12">
+            <div className="bg-[#5A50FE] py-5 px-5 rounded-full flex items-center justify-center h-[4.5rem] w-[4.5rem] ">
+              <img src={team} alt="" className="w-fit" />
+            </div>
+            <div className="mt-2">
+              <p>Large Corporations</p>
+            </div>
+          </div>
         </div>
     </div>
   </div>
