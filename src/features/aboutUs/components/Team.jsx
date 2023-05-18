1a3cc4101d843d80f453c5859e55e6245d813d5b
edgarampiire <aedgar990@gmail.com>
2023-05-18 14:15:31 +0300

    about us - team section

--- src/features/aboutUs/components/Team.jsx
+++ src/features/aboutUs/components/Team.jsx
@@ -3,8 +3,11 @@ import React from "react";
 export const Team = () => {
   return (
   <div>
-    <div>
-        <h1>The Team that makes it happen</h1>
+    <div className="mt-24">
+        <div className="flex justify-center">
+
+        <h1 className="font-semibold text-xl text-[#040052]">The Team that makes it happen</h1>
+        </div>
     </div>
   </div>
     )
--- src/features/aboutUs/routes/AboutUs.jsx
+++ src/features/aboutUs/routes/AboutUs.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { MainLayout } from "../../../components/layout/MainLayout";
 import { Hero } from "../components/Hero";
 import { Glance } from "../components/Glance";
+import { Team } from "../components/Team";
 
 export const AboutUs = () => {
   return (
@@ -9,6 +10,7 @@ export const AboutUs = () => {
       <MainLayout page="about-us">
         <Hero />
         <Glance />
+        <Team />
       </MainLayout>
     </div>
   );
