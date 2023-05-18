fb30c6f99d3098e18e6374b3dd48c5cf26b9d4ed
edgarampiire <aedgar990@gmail.com>
2023-05-18 14:21:30 +0300

    about us - team section

--- src/features/aboutUs/components/Team.jsx
+++ src/features/aboutUs/components/Team.jsx
@@ -2,13 +2,35 @@ import React from "react";
 
 export const Team = () => {
   return (
-  <div>
-    <div className="mt-24">
+    <div>
+      <div className="mt-24">
         <div className="flex justify-center">
-
-        <h1 className="font-semibold text-xl text-[#040052]">The Team that makes it happen</h1>
+          <h1 className="font-semibold text-xl text-[#040052]">
+            The Team that makes it happen
+          </h1>
         </div>
+        <div className="w-10/12 ">
+          <div>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <div>
+              <div>
+                <h1>E Michael Rosales.MBA, CSPO, CPIP, CSM</h1>
+                <p>Chief Digital Product Consultant, BSG Co-founder</p>
+              </div>
+              <div>
+                <p>
+                  Michael is an innovative, value-centric, outcome-driven and
+                  entrepreneurial business strategist, digital product leader,
+                  thinker, and problem solver with Digital DNA leveraging 15+
+                  years of product and business strategy experience.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-    )
+  );
 };
