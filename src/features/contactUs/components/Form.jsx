2467e84e487ada87c794c13c118214d775c7db08
edgarampiire <aedgar990@gmail.com>
2023-05-18 18:41:06 +0300

    contact us - team form

--- src/features/contactUs/components/Form.jsx
+++ src/features/contactUs/components/Form.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import {
+  MdOutlineEmail,
+  MdOutlineLocationOn,
+  MdOutlineLocalPhone,
+} from "react-icons/md";
 
 export const Form = () => {
   return (
@@ -10,18 +15,37 @@ export const Form = () => {
           </h1>
         </div>
 
-        <div>
-          <div>Send</div>
-          <div>
+        <div className="w-full flex mt-10">
+          <div className="w-5/12">
+            <div className="px-1 py-2 gap-5 flex flex-col">
+              <div>
+                <div className="flex gap-2">
+                  <div className="flex py-3 px-3 bg-[#0055BA] rounded-full w-fit">
+                    <MdOutlineEmail className="text-white" />
+                  </div>
+                  <div className="text-sm">
+
+                    <p className="text-[#040052] font-semibold">Send us an email</p>
+                    <p className="text-[#040052]">info@theblueprintstrategy.com</p>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <MdOutlineLocationOn />
+              </div>
+              <div>
+                <MdOutlineLocalPhone />
+              </div>
+            </div>
+          </div>
+          <div className="w-7/12">
             <form>
               <div>
                 <label>First Name</label>
                 <input type="text" placeholder="firstname" />
                 <label>Last Name</label>
                 <input type="text" placeholder="lastname" />
-
               </div>
-
             </form>
           </div>
         </div>
