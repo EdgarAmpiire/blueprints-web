c9b6b0a44bac60cb691499bf34ec38dfa1db38d3
edgarampiire <aedgar990@gmail.com>
2023-05-18 12:15:20 +0300

    about us - hero

--- src/features/aboutUs/components/Hero.jsx
+++ src/features/aboutUs/components/Hero.jsx
@@ -5,9 +5,9 @@ export const Hero = () => {
     <di className="">
       <div
         class="w-full bg-cover bg-center object-cover"
-        style={{ height: "32rem", backgroundImage: `url(/images/bg.png)` }}>
-        <div class="flex flex-col items-center justify-between h-full w-full bg-[#000000D9] opacity-70">
-          <div class="text-center flex flex-col items-center  mt-10">
+        style={{ height: "28rem", backgroundImage: `url(/images/bg.png)` }}>
+        <div class="flex flex-col items-center justify-between h-full w-full py-10 bg-[#000000D9] backdrop-brightness-200">
+          <div class="text-center flex flex-col items-center  ">
             <h1 class="text-white text-2xl font-semibold uppercase md:text-3xl">
               We Are
             </h1>
