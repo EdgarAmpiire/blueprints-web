0b911eb3d613e0d07a3cb2f754a7a5be0865eea7
user <user@users-iMac.local>
2023-05-19 13:08:36 +0300

    how we work - hero

--- src/features/howWeWork/components/Hero.jsx
+++ src/features/howWeWork/components/Hero.jsx
@@ -1,40 +1,29 @@
-import React from 'react'
-import { BsArrowRight } from 'react-icons/bs'
-import { Link } from 'react-router-dom'
+import React from "react";
 
 export const Hero = () => {
   return (
     <div className="">
-    <div
-      class="w-full bg-cover bg-center object-cover"
-      style={{ height: "28rem", backgroundImage: `url(/images/chessbg.png)` }}>
-      <div class="flex flex-col items-center justify-between h-full w-full py-10 bg-[#000000D9] backdrop-brightness-200">
-        <div class="text-center flex flex-col items-center  ">
-          <h1 class="text-white text-2xl font-semibold uppercase md:text-3xl">
-            We Are
-          </h1>
-          <h2 className="text-white text-xl font-semibold uppercase mt-10 ">
-            WE ARE BLUEPRINT STRATEGY GROUP, INC
-          </h2>
-
-          <p className="text-white mt-10 w-6/12 font-light">
-             We are  a digital consulting firm that partners with businesses
-            to conceptualize, design, build and position their digital
-            products and services
-          </p>
+      <div
+        class="w-full bg-cover bg-center object-cover"
+        style={{
+          height: "28rem",
+          backgroundImage: `url(/images/chessbg.png)`,
+        }}>
+        <div class="flex flex-col items-center justify-between h-full w-full py-10 bg-[#000000D9] backdrop-brightness-200">
+          <div className="mt-14 flex flex-col items-center justify-center w-full">
+            <h1 className="text-4xl w-7/12 font-semibold text-center text-white">
+              To build a wining blueprint
+            </h1>
+            <p className="w-7/12 text-center mt-14 font-light text-white ">
+               Leverage our consulting methodology that is designed to
+              prioritize and solve the problems that capture and deliver the
+              most value, create scalable market opportunities, jump start
+              digital transformation, and set a clear and measurable path to
+              disruptive growth and profitability
+            </p>
+          </div>
         </div>
-        <Link to="/contact" className="">
-            <div className=" bg-[#E5E5E5] py-3 px-4 rounded-md flex items-center mt-5 ml-0 w-fit gap-2 text-white hover:text-white hover:gap-4 ease-in duration-150   ">
-              <p className="pl-2 pr-2 text-sm text-black ">
-              Want to know how we work
-              </p>
-              <span className="">
-                <BsArrowRight className="text-2xl text-black  " />
-              </span>
-            </div>
-          </Link>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
