c37e8c7773cbf0b5c3801aa5064b46491f5db1ea
user <user@users-iMac.local>
2023-05-19 13:34:05 +0300

    how we work - hero

--- src/features/howWeWork/components/Consultation.jsx
+++ src/features/howWeWork/components/Consultation.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import thumb from "../../../assets/thumb.png"
 
 export const Consultation = () => {
   return (
-    <div>Consultation</div>
+    <div>
+        <div>
+            <div>
+                <h1>Blueprint Consulting 7 Step System</h1>
+            </div>
+            <div>
+                <p>Our 7-step framework is a fundamental method to structure a consulting project that begins with establishing a partnership and relationship through evaluating and acting on the results of the output of our work. The implementation of solutions is iterative and remains transparent for our clients to be informed and updated at each stage of the process</p>
+            </div>
+            <div>
+                <div>
+                    <img src={thumb} alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
--- src/features/howWeWork/routes/HowWeWork.jsx
+++ src/features/howWeWork/routes/HowWeWork.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { MainLayout } from "../../../components/layout/MainLayout";
 import { Hero } from "../components/Hero";
+import { Consultation } from "../components/Consultation";
 
 export const HowWeWork = () => {
   return (
     <div>
       <MainLayout page="how-we-work">
         <Hero />
+        <Consultation />
       </MainLayout>
     </div>
   );
