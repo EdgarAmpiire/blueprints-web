53315056f720aa07546b0d1e338bce0d9bdd4bde
user <user@users-iMac.local>
2023-05-19 15:07:43 +0300

    how we work - hero

--- src/features/howWeWork/components/Principles.jsx
+++ src/features/howWeWork/components/Principles.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const Principles = () => {
   return (
-    <div>Principles</div>
+    <div>
+        <div className='mt-16'>
+            <div className='flex flex-col items-center justify-center'>
+
+            <div className='bg-[#0055BA1A] py-2 px-8 rounded-full '>
+                <h1 className='text-[#0055BA] font-semibold'>Our Guiding Principles</h1>
+            </div>
+            </div>
+        </div>
+    </div>
   )
 }
