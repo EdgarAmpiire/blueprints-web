62b2338e4e9de15b92cb004acab102e310684e73
user <user@users-iMac.local>
2023-05-19 12:04:21 +0300

    resources - page

--- src/features/resources/routes/Resources.jsx
+++ src/features/resources/routes/Resources.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { MainLayout } from '../../../components/layout/MainLayout'
 
 export const Resources = () => {
   return (
+    <MainLayout page="resources" >
+
     <div>Resources</div>
+    </MainLayout>
   )
 }
--- src/routes/public.jsx
+++ src/routes/public.jsx
@@ -2,6 +2,7 @@ import { AboutUs } from "../features/aboutUs";
 import { ContactUs } from "../features/contactUs";
 import { Home } from "../features/home";
 import { HowWeWork } from "../features/howWeWork";
+import { Resources } from "../features/resources";
 import { Services } from "../features/services";
   
   export const publicRoutes = [
@@ -9,5 +10,6 @@ import { Services } from "../features/services";
     {path: "/services", element: <Services />},
     {path: "/about-us", element: <AboutUs />},
     {path: "/how-we-work", element: <HowWeWork/>},
+    {path: "/resources", element: <Resources/>},
     {path: "/contact-us", element: <ContactUs />}
   ];
