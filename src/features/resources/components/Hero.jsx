700e5e1b8d8425d52d9d6f58c5d2ff34043368d0
user <user@users-iMac.local>
2023-05-19 16:52:26 +0300

    resources - hero

--- src/features/resources/components/Hero.jsx
+++ src/features/resources/components/Hero.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export const Hero = () => {
   return (
     <div>
-        <div className='mt-16'>
-            <div>
-
-            <div>
-                <button className='bg-[#0055BA] text-white'>Blog</button>
-            <button className='bg-[#0055BA33] text-[#040052]'>
-                Press
-            </button>
-            </div>
-            </div>
+      <div className="mt-16">
+        <div className="w-full">
+          <div className="w-4/12 flex justify-between">
+            <button className="bg-[#0055BA] text-white px-10 py-2 rounded-full">Blog</button>
+            <button className="bg-[#0055BA33] text-[#040052] px-10 py-2 rounded-full">Press</button>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
