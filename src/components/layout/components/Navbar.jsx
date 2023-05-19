16096d76421c45643670dbd3f8e0d340a39c9265
user <user@users-iMac.local>
2023-05-19 17:16:48 +0300

    redo

--- src/components/layout/components/Navbar.jsx
+++ src/components/layout/components/Navbar.jsx
@@ -7,12 +7,12 @@ import { Link } from "react-router-dom";
   
   
     return (
-      <nav className="relative flex flex-wrap items-center justify-between px-0 py-0 ">
+      <nav className="relative flex flex-wrap items-center justify-between px-0 py-5 ">
         <div className="container px-0 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <a
               className="text-sm font-bold leading-relaxed inline-block mr-0 py-2 whitespace-nowrap uppercase text-white"
-              href="#pablo"
+              href="/"
             >
               <Link className="flex items-center text-[#0055BA]" to="/">
                 {/* <img
