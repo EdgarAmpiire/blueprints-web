c77750e6937f9380b2a101ac763832062c4720e7
user <user@users-iMac.local>
2023-05-30 12:57:04 +0300

    responsiveness

--- src/features/home/components/Banner.jsx
+++ src/features/home/components/Banner.jsx
@@ -5,11 +5,11 @@ export const Banner = () => {
   return (
     <div className="flex items-center justify-center bg-[#0055BA] mt-5 ">
 
-    <div className="my-10 md:w-10/12">
+    <div className="my-10 lg:w-10/12 w-12/12">
       <div className="bg-[#0055BA] flex items-center justify-center w-full ">
-        <div className="mx-14 flex mt-5 w-9/12">
+        <div className="lg:mx-14 mx-8 flex mt-5 w-9/12">
           <div className="flex flex-col items-center justify-center w-8/12">
-            <h1 className="font-semibold text-white w-10/12">
+            <h1 className="font-semibold text-white lg:w-10/12">
               “ Let’s make the world a better place as a result of the problems
               that we solve, one sprint at a time. “
             </h1>
