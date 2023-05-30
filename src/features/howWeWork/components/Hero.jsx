d391c1691b132e030b83bbee8e8131ba115f2fd9
user <user@users-iMac.local>
2023-05-30 14:15:22 +0300

    responsiveness

--- src/features/howWeWork/components/Hero.jsx
+++ src/features/howWeWork/components/Hero.jsx
@@ -11,10 +11,10 @@ export const Hero = () => {
         }}>
         <div class="flex flex-col items-center justify-between h-full w-full py-10 bg-[#000000D9] backdrop-brightness-200">
           <div className="mt-14 flex flex-col items-center justify-center w-full">
-            <h1 className="text-4xl w-7/12 font-semibold text-center text-white">
+            <h1 className="text-4xl lg:w-7/12 w-9/12 font-semibold text-center text-white">
               To build a wining blueprint
             </h1>
-            <p className="w-7/12 text-center mt-14 font-light text-white ">
+            <p className="lg:w-7/12 w-9/12 text-center mt-14 font-light text-white ">
                Leverage our consulting methodology that is designed to
               prioritize and solve the problems that capture and deliver the
               most value, create scalable market opportunities, jump start
