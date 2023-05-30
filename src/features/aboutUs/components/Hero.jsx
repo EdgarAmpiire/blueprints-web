4442ef951965378348f7b89c008a214c23bb448c
user <user@users-iMac.local>
2023-05-30 17:25:29 +0300

    responsiveness

--- src/features/aboutUs/components/Hero.jsx
+++ src/features/aboutUs/components/Hero.jsx
@@ -10,14 +10,14 @@ export const Hero = () => {
         style={{ height: "28rem", backgroundImage: `url(/images/bg.png)` }}>
         <div class="flex flex-col items-center justify-between h-full w-full py-10 bg-[#000000D9] backdrop-brightness-200 ">
           <div class="text-center flex flex-col items-center  ">
-            <h1 class="text-white text-2xl font-semibold uppercase md:text-3xl">
+            <h1 class="text-white text-3xl font-semibold uppercase ">
               We Are
             </h1>
             <h2 className="text-white text-xl font-semibold uppercase mt-10 ">
               WE ARE BLUEPRINT STRATEGY GROUP, INC
             </h2>
 
-            <p className="text-white mt-10 w-6/12 font-light">
+            <p className="text-white mt-10 lg:w-8/12 w-6/12 font-light">
                We are  a digital consulting firm that partners with businesses
               to conceptualize, design, build and position their digital
               products and services
