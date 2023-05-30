c03452c6c85fd8cb4719153f423a85ede431c782
user <user@users-iMac.local>
2023-05-30 17:45:50 +0300

    responsiveness

--- src/features/resources/components/Hero.jsx
+++ src/features/resources/components/Hero.jsx
@@ -8,43 +8,43 @@ export const Hero = () => {
     setisActive(item);
   };
   return (
-    <div className="mt-16 ">
-      <div className=" flex items-center justify-center ">
-        <div className="lg:w-10/12 w-11/12">
-          
-
-          <div className="">
-            
-              <div className=" border-gray-300">
-                <div className="flex gap-10 w-4/12 ">
-                  <button
-                    className={
-                      isActive === "blog"
-                        ? "bg-[#0055BA] text-white px-10 py-2 rounded-full"
-                        : "bg-[#0055BA33] text-[#040052] px-10 py-2 rounded-full"
-                    }
-                    onClick={() => changeComponent("blog")}>
-                    Blog
-                  </button>
-                  <button
-                    className={
-                      isActive === "press"
-                        ? "bg-[#0055BA] text-white px-10 py-2 rounded-full"
-                        : "bg-[#0055BA33] text-[#040052] px-10 py-2 rounded-full"
-                    }
-                    onClick={() => changeComponent("press")}>
-                    Press
-                  </button>
+    <div>
+      <div className="flex items-center justify-center">
+        <div className="lg:max-w-7xl max-w-4xl">
+          <div className="mt-16 ">
+            <div className=" flex items-center justify-center ">
+              <div className="lg:w-10/12 w-11/12">
+                <div className="">
+                  <div className=" border-gray-300">
+                    <div className="flex gap-10 w-4/12 ">
+                      <button
+                        className={
+                          isActive === "blog"
+                            ? "bg-[#0055BA] text-white px-10 py-2 rounded-full"
+                            : "bg-[#0055BA33] text-[#040052] px-10 py-2 rounded-full"
+                        }
+                        onClick={() => changeComponent("blog")}>
+                        Blog
+                      </button>
+                      <button
+                        className={
+                          isActive === "press"
+                            ? "bg-[#0055BA] text-white px-10 py-2 rounded-full"
+                            : "bg-[#0055BA33] text-[#040052] px-10 py-2 rounded-full"
+                        }
+                        onClick={() => changeComponent("press")}>
+                        Press
+                      </button>
+                    </div>
+                  </div>
+                  <div className="mt-10">
+                    {isActive === "blog" && <Blogs />}
+                    {isActive === "press" && "presses"}
+                  </div>
                 </div>
               </div>
-              <div className="mt-10">
-                {isActive === "blog" && <Blogs />}
-                {isActive === "press" && "presses"}
-              </div>
-            
+            </div>
           </div>
-
-          
         </div>
       </div>
     </div>
--- src/features/services/components/DigitalLab.jsx
+++ src/features/services/components/DigitalLab.jsx
@@ -5,134 +5,135 @@ export const DigitalLab = () => {
   return (
     <div>
       <div className="flex items-center justify-center">
-          <div className="lg:max-w-7xl max-w-4xl">
-      <div className="mt-20 flex flex-col items-center">
-        <div>
-          <h1 className="text-[#040052] lg:text-3xl text-xl font-semibold">
-            Our Digital Lab{" "}
-          </h1>
-        </div>
+        <div className="lg:max-w-7xl max-w-4xl">
+          <div className="mt-20 flex flex-col items-center">
+            <div>
+              <h1 className="text-[#040052] lg:text-3xl text-xl font-semibold">
+                Our Digital Lab{" "}
+              </h1>
+            </div>
 
-        <div className="bg-[#D8D6FF] rounded-xl py-10 px-10 mt-10 w-11/12 lg:w-10/12">
-          <div className="grid grid-cols-2 w-full">
-            <div className="flex items-center justify-center ">
-              <img src={thumb} alt="" />
-            </div>
-            <div className="flex flex-col items-start ">
-              <div>
-                <h1 className="text-[#040052] text-lg font-semibold">
-                  Business and Product Strategy
-                </h1>
-                <h3 className="mt-5 text-[#040052]">
-                  Investing in a plan for Growth, Revenue, and Profits
-                </h3>
+            <div className="bg-[#D8D6FF] rounded-xl py-10 px-10 mt-10 w-11/12 lg:w-10/12">
+              <div className="grid grid-cols-2 w-full">
+                <div className="flex items-center justify-center ">
+                  <img src={thumb} alt="" />
+                </div>
+                <div className="flex flex-col items-start ">
+                  <div>
+                    <h1 className="text-[#040052] text-lg font-semibold">
+                      Business and Product Strategy
+                    </h1>
+                    <h3 className="mt-5 text-[#040052]">
+                      Investing in a plan for Growth, Revenue, and Profits
+                    </h3>
+                  </div>
+                  <div className="mt-5 w-8/12">
+                    <p className="text-[#6B6A6A] text-sm">
+                      Leverage BSG’s business and product strategy team to
+                      kickstart product thinking at your organization.
+                    </p>
+                    <p className="mt-5 text-[#6B6A6A] text-sm ">Services:</p>
+                    <ul class="list-disc mt-5 text-[#6B6A6A] text-sm ml-8">
+                      <li>Small Business Digital Consulting </li>
+                      <li>Business Strategy Builder </li>
+                      <li>Product Strategy Builder </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
-              <div className="mt-5 w-8/12">
-                <p className="text-[#6B6A6A] text-sm">
-                  Leverage BSG’s business and product strategy team to kickstart
-                  product thinking at your organization.
-                </p>
-                <p className="mt-5 text-[#6B6A6A] text-sm ">Services:</p>
-                <ul class="list-disc mt-5 text-[#6B6A6A] text-sm ml-8">
-                  <li>Small Business Digital Consulting </li>
-                  <li>Business Strategy Builder </li>
-                  <li>Product Strategy Builder </li>
-                </ul>
-              </div>
             </div>
-          </div>
-        </div>
 
-        <div className="bg-[#F5E5FF] rounded-xl py-10 px-10 mt-16 w-11/12 lg:w-10/12">
-          <div className="grid grid-cols-2 w-full">
-            <div className="flex flex-col items-start ">
-              <div>
-                <h1 className="text-[#040052] text-lg font-semibold">
-                  Digital Marketing
-                </h1>
-                <h3 className="mt-5 text-[#040052]">Connect and Convert</h3>
-              </div>
-              <div className="mt-5 w-8/12">
-                <p className="text-[#6B6A6A] text-sm">
-                  A digital marketing strategy is a set of planned, strategic
-                  and tactical actions to acquire and retain the attention of
-                  your addressable market
-                </p>
-                <p className="mt-5 text-[#6B6A6A] text-sm ">Services:</p>
-                <ul class="list-disc mt-5 text-[#6B6A6A] text-sm ml-8">
-                  <li>Digital Marketing Strategy Builder </li>
-                  <li>Digital Marketing Assesment Tools </li>
-                </ul>
+            <div className="bg-[#F5E5FF] rounded-xl py-10 px-10 mt-16 w-11/12 lg:w-10/12">
+              <div className="grid grid-cols-2 w-full">
+                <div className="flex flex-col items-start ">
+                  <div>
+                    <h1 className="text-[#040052] text-lg font-semibold">
+                      Digital Marketing
+                    </h1>
+                    <h3 className="mt-5 text-[#040052]">Connect and Convert</h3>
+                  </div>
+                  <div className="mt-5 w-8/12">
+                    <p className="text-[#6B6A6A] text-sm">
+                      A digital marketing strategy is a set of planned,
+                      strategic and tactical actions to acquire and retain the
+                      attention of your addressable market
+                    </p>
+                    <p className="mt-5 text-[#6B6A6A] text-sm ">Services:</p>
+                    <ul class="list-disc mt-5 text-[#6B6A6A] text-sm ml-8">
+                      <li>Digital Marketing Strategy Builder </li>
+                      <li>Digital Marketing Assesment Tools </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="flex items-center justify-center ">
+                  <img src={thumb} alt="" />
+                </div>
               </div>
             </div>
-            <div className="flex items-center justify-center ">
-              <img src={thumb} alt="" />
-            </div>
-          </div>
-        </div>
 
-        <div className="bg-[#F3F3F3] rounded-xl py-10 px-10 mt-10 w-11/12 lg:w-10/12">
-          <div className="grid grid-cols-2 w-full">
-            <div className="flex items-center justify-center ">
-              <img src={thumb} alt="" />
-            </div>
-            <div className="flex flex-col items-start ">
-              <div>
-                <h1 className="text-[#040052] text-lg font-semibold">
-                  Product Development and Product Management Operations
-                  (ProdOps)
-                </h1>
-                <h3 className="mt-5 text-[#040052]">
-                  From Concept to Commercialization
-                </h3>
+            <div className="bg-[#F3F3F3] rounded-xl py-10 px-10 mt-10 w-11/12 lg:w-10/12">
+              <div className="grid grid-cols-2 w-full">
+                <div className="flex items-center justify-center ">
+                  <img src={thumb} alt="" />
+                </div>
+                <div className="flex flex-col items-start ">
+                  <div>
+                    <h1 className="text-[#040052] text-lg font-semibold">
+                      Product Development and Product Management Operations
+                      (ProdOps)
+                    </h1>
+                    <h3 className="mt-5 text-[#040052]">
+                      From Concept to Commercialization
+                    </h3>
+                  </div>
+                  <div className="mt-5 w-8/12">
+                    <p className="text-[#6B6A6A] text-sm">
+                      Product development is the end-to-end process of driving
+                      an idea from concept to commercialization and beyond in
+                      its lifecycle.
+                    </p>
+                    <p className="mt-5 text-[#6B6A6A] text-sm ">Services:</p>
+                    <ul class="list-disc mt-5 text-[#6B6A6A] text-sm ml-8">
+                      <li>Product Development </li>
+                      <li>Product Operations (ProdOps) </li>
+                      <li>Design Thinking and Innovation </li>
+                      <li>User experience (UX) design </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
-              <div className="mt-5 w-8/12">
-                <p className="text-[#6B6A6A] text-sm">
-                  Product development is the end-to-end process of driving an
-                  idea from concept to commercialization and beyond in its
-                  lifecycle.
-                </p>
-                <p className="mt-5 text-[#6B6A6A] text-sm ">Services:</p>
-                <ul class="list-disc mt-5 text-[#6B6A6A] text-sm ml-8">
-                  <li>Product Development </li>
-                  <li>Product Operations (ProdOps) </li>
-                  <li>Design Thinking and Innovation </li>
-                  <li>User experience (UX) design </li>
-                </ul>
-              </div>
             </div>
-          </div>
-        </div>
 
-        <div className="bg-[#ECEBFF] rounded-xl py-10 px-10 mt-16 w-11/12 lg:w-10/12">
-          <div className="grid grid-cols-2 w-full">
-            <div className="flex flex-col items-start ">
-              <div>
-                <h1 className="text-[#040052] text-lg font-semibold">
-                  Product Portfolio Management
-                </h1>
-                <h3 className="mt-5 text-[#040052]">Investing in value</h3>
+            <div className="bg-[#ECEBFF] rounded-xl py-10 px-10 mt-16 w-11/12 lg:w-10/12">
+              <div className="grid grid-cols-2 w-full">
+                <div className="flex flex-col items-start ">
+                  <div>
+                    <h1 className="text-[#040052] text-lg font-semibold">
+                      Product Portfolio Management
+                    </h1>
+                    <h3 className="mt-5 text-[#040052]">Investing in value</h3>
+                  </div>
+                  <div className="mt-5 w-8/12">
+                    <p className="text-[#6B6A6A] text-sm">
+                      Leverage BSG’s business and product strategy team to
+                      kickstart product thinking at your organization.
+                    </p>
+                    <p className="mt-5 text-[#6B6A6A] text-sm ">Services:</p>
+                    <ul class="list-disc mt-5 text-[#6B6A6A] text-sm ml-8">
+                      <li>Small Business Digital Consulting </li>
+                      <li>Business Strategy Builder </li>
+                      <li>Product Strategy Builder </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="flex items-center justify-center ">
+                  <img src={thumb} alt="" />
+                </div>
               </div>
-              <div className="mt-5 w-8/12">
-                <p className="text-[#6B6A6A] text-sm">
-                Leverage BSG’s business and product strategy team to kickstart product thinking at your organization.
-                </p>
-                <p className="mt-5 text-[#6B6A6A] text-sm ">Services:</p>
-                <ul class="list-disc mt-5 text-[#6B6A6A] text-sm ml-8">
-                  <li>Small Business Digital Consulting </li>
-                  <li>Business Strategy Builder </li>
-                  <li>Product Strategy Builder </li>
-                </ul>
-              </div>
-            </div>
-            <div className="flex items-center justify-center ">
-              <img src={thumb} alt="" />
             </div>
           </div>
         </div>
       </div>
-    </div>
-    </div>
     </div>
   );
 };
