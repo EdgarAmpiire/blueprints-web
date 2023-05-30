14de5c16adb10d9e2134d83c31774891f789ad99
user <user@users-iMac.local>
2023-05-30 14:37:33 +0300

    responsiveness

--- src/features/resources/components/Hero.jsx
+++ src/features/resources/components/Hero.jsx
@@ -3,9 +3,9 @@ import thumb from "../../../assets/thumb.png";
 
 export const Hero = () => {
   return (
-    <div>
-      <div className="mt-16">
-        <div className="w-full">
+    <div className="mt-16 ">
+      <div className=" flex items-center justify-center ">
+        <div className="lg:w-10/12 w-11/12">
           <div className="w-4/12 flex justify-between">
             <button className="bg-[#0055BA] text-white px-10 py-2 rounded-full">
               Blog
