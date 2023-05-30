9a0f2e05e04f67e4986c750e0414d9c92529eaf1
user <user@users-iMac.local>
2023-05-30 13:05:15 +0300

    responsiveness

--- src/features/home/components/HomeBlog.jsx
+++ src/features/home/components/HomeBlog.jsx
@@ -4,14 +4,14 @@ import { Carousel } from "./Carousel";
 export const HomeBlog = () => {
   return (
     <div className="flex justify-center items-center">
-      <div className="w-10/12 md:w-10/12">
+      <div className="lg:w-10/12 w-11/12">
 
       <div className="mt-24 flex flex-col ">
         <div className="w-full flex justify-start ">
           <div className="px-6 py-1 bg-primary"></div>
         </div>
-        <div className="w-8/12 flex flex-col justify-start mt-4 ">
-          <h1 className="text-3xl text-[#040052] w-9/12 font-semibold">
+        <div className="lg:w-8/12 w-10/12 flex flex-col justify-start mt-4 ">
+          <h1 className="text-3xl text-[#040052] lg:w-9/12 w-8/12 font-semibold">
             People, Process and Technology are always in motion.
           </h1>
           <p className="mt-5 text-lg text-gray-500 ">
