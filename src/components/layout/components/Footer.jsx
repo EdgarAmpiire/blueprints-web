32e6f3455024bebf6ff566401d17e56c7a63c676
Ocan Samuel <ocansamuel@Ocans-MacBook-Pro.local>
2023-05-31 17:53:00 +0300

    responsiveness

--- src/components/layout/components/Footer.jsx
+++ src/components/layout/components/Footer.jsx
@@ -91,7 +91,7 @@ export const Footer = () => {
             </div>
           </div>
 
-          <div className="mt-16 flex items-center justify-center  w-full gap-10 ">
+          <div className="mt-16 hidden lg:flex items-center justify-center  w-full gap-10 ">
             <div className="">
               <h1 className="text-white">
                 2023 © The Blueprint Strategy Group - All Rights Reserved
@@ -102,6 +102,22 @@ export const Footer = () => {
             </div>
             <div className="border-l-2 border-l-white px-5">
               <h1 className="text-white">Terms & Conditions</h1>
+            </div>
+          </div>
+
+          <div className="mt-16 flex items-center justify-center  w-full gap-10 ">
+            <div className="">
+              <h1 className="text-white">
+                2023 © The Blueprint Strategy Group - All Rights Reserved
+              </h1>
+            </div>
+            <div>
+              <div className="border-l-2 border-l-white px-5">
+                <h1 className="text-white">Privacy Policy</h1>
+              </div>
+              <div className="border-l-2 border-l-white px-5">
+                <h1 className="text-white">Terms & Conditions</h1>
+              </div>
             </div>
           </div>
         </div>
