b4568e1739fd06294bdfbebe81323dfd3fc79196
Ocan Samuel <ocansamuel@Ocans-MacBook-Pro.local>
2023-05-31 12:56:12 +0300

    responsivness

--- src/features/home/components/Clients.jsx
+++ src/features/home/components/Clients.jsx
@@ -5,27 +5,26 @@ export const Clients = () => {
   return (
     <div>
       <div className="flex items-center justify-center">
-          <div className="lg:max-w-7xl max-w-4xl">
-         
-    <div className="flex justify-center items-center w-12/12 ">
-      <div className="mt-24 lg:w-10/12 w-11/12">
-        <div className="w-full flex justify-start ">
-          <div className="px-6 py-1 bg-primary"></div>
-        </div>
-        <div className="w-full flex justify-start mt-4 ">
-          <h1 className="text-lg text-gray-500">
-            Trusted by select clients, companies & partners
-          </h1>
-        </div>
+        <div className="lg:max-w-7xl max-w-4xl">
+          <div className="flex justify-center items-center w-12/12 ">
+            <div className="mt-24 lg:w-10/12 w-11/12">
+              <div className="w-full flex justify-start ">
+                <div className="px-6 py-1 bg-primary"></div>
+              </div>
+              <div className="w-full flex justify-start mt-4 ">
+                <h1 className="text-lg text-gray-500">
+                  Trusted by select clients, companies & partners
+                </h1>
+              </div>
 
-        {/* slider  */}
-        <div>
-          <ClientSlider />
+              {/* slider  */}
+              <div>
+                <ClientSlider />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    </div>
-          </div>
     </div>
   );
 };
--- src/features/home/components/HomeBlog.jsx
+++ src/features/home/components/HomeBlog.jsx
@@ -3,29 +3,34 @@ import { Carousel } from "./Carousel";
 
 export const HomeBlog = () => {
   return (
-    <div className="flex justify-center items-center">
-      <div className="lg:w-10/12 w-11/12">
-
-      <div className="mt-24 flex flex-col ">
-        <div className="w-full flex justify-start ">
-          <div className="px-6 py-1 bg-primary"></div>
-        </div>
-        <div className="lg:w-8/12 w-10/12 flex flex-col justify-start mt-4 ">
-          <h1 className="text-3xl text-[#040052] lg:w-9/12 w-8/12 font-semibold">
-            People, Process and Technology are always in motion.
-          </h1>
-          <p className="mt-5 text-lg text-gray-500 ">
-            Ultimately, these three interdependent elements are a key strategic
-            component for digital operations to reach consistent efficiency,
-            automated scaling, and achieve goals and outcomes
-          </p>
+    <div>
+      <div className="flex items-center justify-center">
+        <div className="lg:max-w-7xl max-w-4xl">
+          <div className="flex justify-center items-center">
+            <div className="lg:w-10/12 w-11/12">
+              <div className="mt-24 flex flex-col ">
+                <div className="w-full flex justify-start ">
+                  <div className="px-6 py-1 bg-primary"></div>
+                </div>
+                <div className="lg:w-8/12 w-10/12 flex flex-col justify-start mt-4 ">
+                  <h1 className="text-3xl text-[#040052] lg:w-9/12 w-8/12 font-semibold">
+                    People, Process and Technology are always in motion.
+                  </h1>
+                  <p className="mt-5 text-lg text-gray-500 ">
+                    Ultimately, these three interdependent elements are a key
+                    strategic component for digital operations to reach
+                    consistent efficiency, automated scaling, and achieve goals
+                    and outcomes
+                  </p>
+                </div>
+              </div>
+              <div className="mb-10">
+                <Carousel />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="mb-10">
-        <Carousel />
-      </div>
     </div>
-    </div>
-
   );
 };
