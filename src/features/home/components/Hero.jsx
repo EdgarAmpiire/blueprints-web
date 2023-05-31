bd18377d939189c5f1b9e78ce913d20e874b1ab9
Ocan Samuel <ocansamuel@Ocans-MacBook-Pro.local>
2023-05-31 12:47:55 +0300

    responsivness

--- src/features/home/components/Hero.jsx
+++ src/features/home/components/Hero.jsx
@@ -7,40 +7,40 @@ export const Hero = () => {
   return (
     <div className="">
       <div className="flex items-center justify-center">
-          <div className="lg:max-w-7xl max-w-4xl">
-      <div className="flex  mt-12 md:justify-center md:items-center ">
-        <div className="  lg:w-10/12 w-11/12 flex flex-col md:flex-row ">
-          <div className="w-8/12   ">
-            <div className="lg:w-10/12 w-12/12">
-              <h1 className="lg:text-4xl text-3xl font-semibold tracking-wide lg:w-9/12 w-10/12 leading-tight text-black">
-                Delivering Solutions Powered by{" "}
-                <span className="text-[#0055BA]"> People, Process,</span> and{" "}
-                <span className="text-[#0055BA]"> Technology</span>
-              </h1>
-              <p className="mt-5 text-gray-500 text-lg">
-                We partners with businesses and product teams to conceptualize,
-                design, build and position their digital products and services
-                to scale and succeed in existing or new markets in the digital
-                economy.
-              </p>
-              <Link to="/contact" className="">
-                <div className=" bg-[#E5E5E5] py-3 px-4 rounded-md flex items-center lg:mt-5 mt-12 ml-0 w-fit gap-2 text-white hover:text-white hover:gap-4 ease-in duration-150   ">
-                  <p className="pl-2 pr-2 text-base text-black ">
-                    Book an initial session now
+        <div className="lg:max-w-7xl max-w-4xl">
+          <div className="flex  mt-12 md:justify-center md:items-center ">
+            <div className="  lg:w-10/12 w-11/12 flex flex-col md:flex-row ">
+              <div className="w-8/12   ">
+                <div className="lg:w-10/12 w-12/12">
+                  <h1 className="lg:text-4xl text-3xl font-semibold tracking-wide lg:w-9/12 w-10/12 leading-tight text-black">
+                    Delivering Solutions Powered by{" "}
+                    <span className="text-[#0055BA]"> People, Process,</span>{" "}
+                    and <span className="text-[#0055BA]"> Technology</span>
+                  </h1>
+                  <p className="mt-5 text-gray-500 text-lg">
+                    We partners with businesses and product teams to
+                    conceptualize, design, build and position their digital
+                    products and services to scale and succeed in existing or
+                    new markets in the digital economy.
                   </p>
-                  <span className="">
-                    <BsArrowRight className="text-2xl text-black  " />
-                  </span>
+                  <Link to="/contact" className="">
+                    <div className=" bg-[#E5E5E5] py-3 px-4 rounded-md flex items-center lg:mt-5 mt-12 ml-0 w-fit gap-2 text-white hover:text-white hover:gap-4 ease-in duration-150   ">
+                      <p className="pl-2 pr-2 text-base text-black ">
+                        Book an initial session now
+                      </p>
+                      <span className="">
+                        <BsArrowRight className="text-2xl text-black  " />
+                      </span>
+                    </div>
+                  </Link>
                 </div>
-              </Link>
+              </div>
+              <div className="w-4/12 flex items-center justify-center">
+                <img src={thumb} alt="" />
+              </div>
             </div>
           </div>
-          <div className="w-4/12 flex items-center justify-center">
-            <img src={thumb} alt="" />
-          </div>
         </div>
-      </div>
-      </div>
       </div>
     </div>
   );
--- src/features/home/components/HomeServices.jsx
+++ src/features/home/components/HomeServices.jsx
@@ -13,15 +13,17 @@ export const HomeServices = () => {
   // };
   return (
     <div>
-      <div className="mt-24">
-        <div className="w-full flex justify-center ">
-          <div className="px-6 py-1 bg-primary"></div>
-        </div>
-        <div className="w-full flex justify-center mt-4 ">
-          <h1 className="text-3xl font-semibold ">We help you with</h1>
-        </div>
+      <div className="flex items-center justify-center">
+        <div className="lg:max-w-7xl max-w-4xl">
+          <div className="mt-24">
+            <div className="w-full flex justify-center ">
+              <div className="px-6 py-1 bg-primary"></div>
+            </div>
+            <div className="w-full flex justify-center mt-4 ">
+              <h1 className="text-3xl font-semibold ">We help you with</h1>
+            </div>
 
-        {/* <div className="bg-[#F5F5F5] py-5 px-3 rounded-lg mt-5">
+            {/* <div className="bg-[#F5F5F5] py-5 px-3 rounded-lg mt-5">
           <div className="py-3">
             <div className=" border-gray-300">
               <div className="flex justify-around w-full ">
@@ -76,61 +78,60 @@ export const HomeServices = () => {
           </div> 
         </div> */}
 
-<div className="flex justify-center items-center">
+            <div className="flex justify-center items-center">
+              <div className=" lg:w-10/12 w-11/12   py-5 lg:px-5 rounded-lg mt-5 gap-10 grid grid-cols-2">
+                <div className="w-12/12 flex justify-center items-center">
+                  <div className="rounded-3xl bg-[#050422] py-8 px-5 h-56 max-h-56 min-h-56 ">
+                    <div>
+                      <img src={strategy} alt="" />
+                    </div>
+                    <div>
+                      <h1 className="text-white mt-5 font-semibold">
+                        Business & Product Strategy
+                      </h1>
+                      <p className="text-white mt-5 font-light text-sm mb-5">
+                        Defining product ideas to achieve your strategic vision
+                        and goals.
+                      </p>
+                    </div>
+                  </div>
+                </div>
 
-        <div className=" lg:w-10/12 w-11/12   py-5 lg:px-5 rounded-lg mt-5 gap-10 grid grid-cols-2">
-          <div className="w-12/12 flex justify-center items-center">
-            <div className="rounded-3xl bg-[#050422] py-8 px-5 h-56 max-h-56 min-h-56 ">
-              <div>
-                <img src={strategy} alt="" />
-              </div>
-              <div>
-                <h1 className="text-white mt-5 font-semibold">
-                  Business & Product Strategy
-                </h1>
-                <p className="text-white mt-5 font-light text-sm mb-5">
-                  Defining product ideas to achieve your strategic vision and
-                  goals.
-                </p>
-              </div>
-            </div>
-          </div>
+                <div className="w-12/12 flex justify-center items-center">
+                  <div className="rounded-3xl bg-[#F3F3F3] py-8 px-5 h-56 max-h-56 min-h-56 ">
+                    <div>
+                      <img src={portfolio} alt="" />
+                    </div>
+                    <div>
+                      <h1 className="text-black mt-5 font-semibold">
+                        Product Portfolio Management
+                      </h1>
+                      <p className="text-black mt-5 font-light text-sm mb-5">
+                        Optimizing resource allocation, aligning products, and
+                        improving portfolios.
+                      </p>
+                    </div>
+                  </div>
+                </div>
 
-          <div className="w-12/12 flex justify-center items-center">
-            <div className="rounded-3xl bg-[#F3F3F3] py-8 px-5 h-56 max-h-56 min-h-56 ">
-              <div>
-                <img src={portfolio} alt="" />
-              </div>
-              <div>
-                <h1 className="text-black mt-5 font-semibold">
-                Product Portfolio Management
-                </h1>
-                <p className="text-black mt-5 font-light text-sm mb-5">
-                Optimizing resource allocation, aligning products, and improving portfolios.
-                </p>
-              </div>
-            </div>
-          </div>
+                <div className="w-12/12  flex justify-center items-center ">
+                  <div className="rounded-3xl bg-[#F3F3F3] py-7 px-5 h-56 max-h-56 min-h-56 ">
+                    <div>
+                      <img src={management} alt="" />
+                    </div>
+                    <div>
+                      <h1 className="text-black mt-3 font-semibold w-10/12">
+                        Product Development and Management Operation
+                      </h1>
+                      <p className="text-black mt-2 font-light text-sm mb-5 w-10/12">
+                        Commercializing new or existing products that meet
+                        market needs
+                      </p>
+                    </div>
+                  </div>
+                </div>
 
-          <div className="w-12/12  flex justify-center items-center ">
-            <div className="rounded-3xl bg-[#F3F3F3] py-7 px-5 h-56 max-h-56 min-h-56 ">
-              <div>
-                <img src={management} alt="" />
-              </div>
-              <div>
-                <h1 className="text-black mt-3 font-semibold w-10/12">
-                                Product Development and Management Operation 
-
-                </h1>
-                <p className="text-black mt-2 font-light text-sm mb-5 w-10/12">
-                Commercializing new or existing products that meet market needs
-
-                </p>
-              </div>
-            </div>
-          </div>
-
-          {/* <div className="w-12/12 flex justify-center items-center">
+                {/* <div className="w-12/12 flex justify-center items-center">
             <div className="rounded-3xl bg-[#F3F3F3] py-8 px-5  ">
               <div>
                 <img src={management} alt="" />
@@ -146,24 +147,26 @@ export const HomeServices = () => {
             </div>
           </div> */}
 
-          <div className="w-12/12 flex justify-center items-center">
-            <div className="rounded-3xl bg-[#050422] py-8 px-5 h-56 max-h-56 min-h-56 ">
-              <div>
-                <img src={marketing} alt="" />
-              </div>
-              <div>
-                <h1 className="text-white mt-5 font-semibold">
-                Digital Marketing
-                </h1>
-                <p className="text-white mt-5 font-light text-sm mb-5">
-                Executing campaigns to increase revenue and engage with your audience
-                </p>
+                <div className="w-12/12 flex justify-center items-center">
+                  <div className="rounded-3xl bg-[#050422] py-8 px-5 h-56 max-h-56 min-h-56 ">
+                    <div>
+                      <img src={marketing} alt="" />
+                    </div>
+                    <div>
+                      <h1 className="text-white mt-5 font-semibold">
+                        Digital Marketing
+                      </h1>
+                      <p className="text-white mt-5 font-light text-sm mb-5">
+                        Executing campaigns to increase revenue and engage with
+                        your audience
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-</div>
-
       </div>
     </div>
   );
