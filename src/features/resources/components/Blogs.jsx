e6674e1a9b2fa5db2c3e53f200f9ca08ae248662
user <user@users-iMac.local>
2023-06-01 15:46:16 +0300

    responsiveness

--- src/features/resources/components/Blogs.jsx
+++ src/features/resources/components/Blogs.jsx
@@ -5,11 +5,11 @@ import thumb from "../../../assets/thumb.png";
 export const Blogs = () => {
   return (
     <div>
-        <div className="lg:flex-row md:flex-row flex-col gap-8 lg:mt-10 md:mt-10 w-full">
-            <div className="lg:w-4/12 md:w-4/12 w-12/12 h-56 bg-[#F3F3F3] flex items-center justify-center rounded-xl">
+        <div className="lg:flex-row md:flex-row flex-col gap-8 lg:mt-10 md:mt-10 w-full lg:flex-none lg:items-start lg:justify-start md:flex-none md:items-start md:justify-start items-center justify-center flex">
+            <div className="lg:w-4/12 md:w-4/12 w-11/12 h-56 bg-[#F3F3F3] flex items-center justify-center rounded-xl">
               <img src={thumb} alt="" className="w-3/12" />
             </div>
-            <div className="lg:w-8/12 md:w-8/12 w-12/12 flex flex-col justify-between py-2 ">
+            <div className="lg:w-8/12 md:w-8/12 w-11/12 flex flex-col justify-between py-2 lg:flex-none lg:items-start lg:justify-start md:flex-none md:items-start md:justify-start items-center ">
               <div className="lg:w-10/12">
                 <h6 className="text-[#04005266] text-sm">
                   18 May 2023, 5 mins read
@@ -23,13 +23,18 @@ export const Blogs = () => {
                   do eiusmod tempor incididunt ut labore et.
                 </p>
               </div>
-              <div>
-                <button className="bg-[#0055BA33] text-[#040052] px-10 py-2 rounded-full">
+              <div className='lg:flex md:flex hidden'>
+                <button className="bg-[#0055BA33] text-[#040052] px-10 py-2 rounded-full ">
                   Read
                 </button>
               </div>
             </div>
           </div>
+          <div className='lg:hidden md:hidden flex px-8 mt-5'>
+                <button className="bg-[#0055BA33] text-[#040052] px-10 py-2 rounded-full ">
+                  Read
+                </button>
+              </div>
           <div className="border-b mt-7"></div>
     </div>
   )
