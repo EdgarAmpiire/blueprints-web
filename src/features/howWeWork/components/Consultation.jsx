e34cf03d70c2c85a44bacb17ec4fe34fd0dc8b2b
user <user@users-iMac.local>
2023-06-01 14:11:14 +0300

    responsiveness

--- src/features/howWeWork/components/Consultation.jsx
+++ src/features/howWeWork/components/Consultation.jsx
@@ -7,14 +7,14 @@ export const Consultation = () => {
       <div className="flex items-center justify-center">
         <div className="lg:max-w-7xl max-w-4xl">
           <div className="mt-16 flex justify-center items-center">
-            <div className="lg:w-10/12 w-11/12 bg-[#ECEBFF] rounded-lg flex flex-col items-center justify-center py-10 px-10">
-              <div className="bg-[#0055BA] py-3 px-10 rounded-full ">
-                <h1 className="text-white">
+            <div className="lg:w-10/12 w-11/12 bg-[#ECEBFF] rounded-lg flex flex-col items-center justify-center py-10 lg:px-10 md:px-10 px-5">
+              <div className="bg-[#0055BA] py-3 lg:px-10 md:px-10 px-5 rounded-full ">
+                <h1 className="text-white lg:text-base md:text-base text-sm">
                   Blueprint Consulting 7 Step System
                 </h1>
               </div>
               <div className="mt-10">
-                <p className="text-[#040052] text-center">
+                <p className="text-[#040052] text-center lg:leading-normal md:leading-normal leading-loose lg:text-base md:text-base text-sm">
                   Our 7-step framework is a fundamental method to structure a
                   consulting project that begins with establishing a partnership
                   and relationship through evaluating and acting on the results
