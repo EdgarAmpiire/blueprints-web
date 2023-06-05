fe7d99a89060f6ea66568dc9d3bd678baa31ced4
Ocan Samuel <ocansamuel@Ocans-MacBook-Pro.local>
2023-06-05 15:32:01 +0300

    scroll effect

--- src/features/services/components/DigitalLab.jsx
+++ src/features/services/components/DigitalLab.jsx
@@ -68,16 +68,31 @@ export const DigitalLab = () => {
               </div>
             </div>
 
-            <div className="bg-[#F5E5FF] rounded-xl py-10 lg:px-10 md:px-10 px-5 mt-10 w-11/12 lg:w-10/12">
+            <div 
+            data-aos="fade-up"
+            data-aos-offset="200"
+            data-aos-easing="ease-in-sine"
+            data-aos-duration="1000"
+            className="bg-[#F5E5FF] rounded-xl py-10 lg:px-10 md:px-10 px-5 mt-10 w-11/12 lg:w-10/12">
               <div className="lg:grid lg:grid-cols-2 md:grid md:grid-cols-2 flex flex-col lg:gap-0 md:gap-0 gap-10 w-full">
                 <div className="flex flex-col items-start ">
-                  <div>
+                  <div
+                  data-aos="fade-up"
+                  data-aos-offset="200"
+                  data-aos-easing="ease-in-sine"
+                  data-aos-duration="1000"
+                  >
                     <h1 className="text-[#040052] text-lg font-semibold">
                       Digital Marketing
                     </h1>
                     <h3 className="mt-5 text-[#040052]">Connect and Convert</h3>
                   </div>
-                  <div className="mt-5 w-8/12">
+                  <div
+                  data-aos="fade-up"
+                  data-aos-offset="200"
+                  data-aos-easing="ease-in-sine"
+                  data-aos-duration="1000"
+                  className="mt-5 w-8/12">
                     <p className="text-[#6B6A6A] text-sm">
                       A digital marketing strategy is a set of planned,
                       strategic and tactical actions to acquire and retain the
@@ -91,18 +106,38 @@ export const DigitalLab = () => {
                   </div>
                 </div>
                 <div className="flex items-center justify-center ">
-                  <img src={thumb} alt="" />
+                  <img
+                  data-aos="fade-left"
+                  data-aos-offset="200"
+                  data-aos-easing="ease-in-sine"
+                  data-aos-duration="1000"
+                  src={thumb} alt="" />
                 </div>
               </div>
             </div>
 
-            <div className="bg-[#F3F3F3] rounded-xl py-10 lg:px-10 md:px-10 px-5 mt-10 w-11/12 lg:w-10/12">
+            <div
+            data-aos="fade-up"
+            data-aos-offset="200"
+            data-aos-easing="ease-in-sine"
+            data-aos-duration="1000"
+            className="bg-[#F3F3F3] rounded-xl py-10 lg:px-10 md:px-10 px-5 mt-10 w-11/12 lg:w-10/12">
               <div className="lg:grid lg:grid-cols-2 md:grid md:grid-cols-2 flex flex-col-reverse lg:gap-0 md:gap-0 gap-10 w-full">
                 <div className="flex items-center justify-center ">
-                  <img src={thumb} alt="" />
+                  <img 
+                  data-aos="fade-right"
+                  data-aos-offset="200"
+                  data-aos-easing="ease-in-sine"
+                  data-aos-duration="1000"
+                  src={thumb} alt="" />
                 </div>
                 <div className="flex flex-col items-start ">
-                  <div>
+                  <div
+                  data-aos="fade-up"
+                  data-aos-offset="200"
+                  data-aos-easing="ease-in-sine"
+                  data-aos-duration="1000"
+                  >
                     <h1 className="text-[#040052] text-lg font-semibold">
                       Product Development and Product Management Operations
                       (ProdOps)
@@ -111,7 +146,12 @@ export const DigitalLab = () => {
                       From Concept to Commercialization
                     </h3>
                   </div>
-                  <div className="mt-5 w-8/12">
+                  <div
+                  data-aos="fade-up"
+                  data-aos-offset="200"
+                  data-aos-easing="ease-in-sine"
+                  data-aos-duration="1000"
+                  className="mt-5 w-8/12">
                     <p className="text-[#6B6A6A] text-sm">
                       Product development is the end-to-end process of driving
                       an idea from concept to commercialization and beyond in
@@ -129,16 +169,31 @@ export const DigitalLab = () => {
               </div>
             </div>
 
-            <div className="bg-[#ECEBFF] rounded-xl py-10 lg:px-10 md:px-10 px-5 mt-10 w-11/12 lg:w-10/12">
+            <div
+            data-aos="fade-up"
+            data-aos-offset="200"
+            data-aos-easing="ease-in-sine"
+            data-aos-duration="1000"
+            className="bg-[#ECEBFF] rounded-xl py-10 lg:px-10 md:px-10 px-5 mt-10 w-11/12 lg:w-10/12">
               <div className="lg:grid lg:grid-cols-2 md:grid md:grid-cols-2 flex flex-col lg:gap-0 md:gap-0 gap-10 w-full">
                 <div className="flex flex-col items-start ">
-                  <div>
+                  <div
+                  data-aos="fade-up"
+                  data-aos-offset="200"
+                  data-aos-easing="ease-in-sine"
+                  data-aos-duration="1000"
+                  >
                     <h1 className="text-[#040052] text-lg font-semibold">
                       Product Portfolio Management
                     </h1>
                     <h3 className="mt-5 text-[#040052]">Investing in value</h3>
                   </div>
-                  <div className="mt-5 w-8/12">
+                  <div
+                  data-aos="fade-up"
+                  data-aos-offset="200"
+                  data-aos-easing="ease-in-sine"
+                  data-aos-duration="1000"
+                  className="mt-5 w-8/12">
                     <p className="text-[#6B6A6A] text-sm">
                       Leverage BSG’s business and product strategy team to
                       kickstart product thinking at your organization.
@@ -152,7 +207,12 @@ export const DigitalLab = () => {
                   </div>
                 </div>
                 <div className="flex items-center justify-center ">
-                  <img src={thumb} alt="" />
+                  <img
+                  data-aos="fade-left"
+                  data-aos-offset="200"
+                  data-aos-easing="ease-in-sine"
+                  data-aos-duration="1000"
+                  src={thumb} alt="" />
                 </div>
               </div>
             </div>
