c8ff7b577ac5a05daaf38f78ff7b5f786944e4ce
Ocan Samuel <ocansamuel@Ocans-MacBook-Pro.local>
2023-06-05 15:26:33 +0300

    scroll effect

--- src/features/services/components/Hero.jsx
+++ src/features/services/components/Hero.jsx
@@ -44,82 +44,83 @@ export const Hero = () => {
         </div>
       </div>
 
-      
-          <div className="flex flex-col items-center mt-16 w-full">
+      <div className="flex flex-col items-center mt-16 w-full">
+        <div
+          data-aos="fade-up"
+          data-aos-offset="200"
+          data-aos-easing="ease-in-sine"
+          data-aos-duration="1000">
+          <h1 className="text-[#040052] font-semibold lg:text-3xl text-xl">
+            We Help With
+          </h1>
+        </div>
+        <div className="flex lg:flex-row md:flex-row flex-col justify-between lg:items-start md:items-start items-center lg:w-7/12 w-10/12 gap-10 mt-14">
+          <div className="flex items-center justify-center flex-col lg:w-4/12 md:w-4/12 w-8/12">
             <div
-            data-aos="fade-up"
-            data-aos-offset="200"
-            data-aos-easing="ease-in-sine"
-            data-aos-duration="1000"
-            >
-              <h1 className="text-[#040052] font-semibold lg:text-3xl text-xl">
-                We Help With
-              </h1>
+              data-aos="fade-up"
+              data-aos-offset="200"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="800"
+              className="bg-[#5A50FE] py-5 px-5 rounded-full flex items-center justify-center h-[4.5rem] w-[4.5rem] ">
+              <img src={startUp} alt="" className="w-fit" />
             </div>
-            <div className="flex lg:flex-row md:flex-row flex-col justify-between lg:items-start md:items-start items-center lg:w-7/12 w-10/12 gap-10 mt-14">
-              <div className="flex items-center justify-center flex-col lg:w-4/12 md:w-4/12 w-8/12">
-                <div
-                data-aos="fade-up"
-                data-aos-offset="200"
-                data-aos-easing="ease-in-sine"
-                data-aos-duration="800"
-                className="bg-[#5A50FE] py-5 px-5 rounded-full flex items-center justify-center h-[4.5rem] w-[4.5rem] ">
-                  <img src={startUp} alt="" className="w-fit" />
-                </div>
-                <div
-                data-aos="fade-up"
-                data-aos-offset="200"
-                data-aos-easing="ease-in-sine"
-                data-aos-duration="800"
-                className="mt-2">
-                  <p>Start-ups</p>
-                </div>
-              </div>
+            <div
+              data-aos="fade-up"
+              data-aos-offset="200"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="800"
+              className="mt-2">
+              <p>Start-ups</p>
+            </div>
+          </div>
 
-              <div className="flex items-center justify-center flex-col lg:w-4/12 md:w-4/12 w-8/12">
-                <div
-                data-aos="fade-up"
-                data-aos-offset="200"
-                data-aos-easing="ease-in-sine"
-                data-aos-duration="900"
-                className="bg-[#5A50FE] py-5 px-5 rounded-full flex items-center justify-center h-[4.5rem] w-[4.5rem] ">
-                  <img src={midSized} alt="" className="w-fit" />
-                </div>
-                <div
-                
-                data-aos="fade-up"
-                  data-aos-offset="200"
-                  data-aos-easing="ease-in-sine"
-                  data-aos-duration="900"
-                  className="mt-2">
-                  <p>Mid Sized Enterprises</p>
-                </div>
-              </div>
+          <div className="flex items-center justify-center flex-col lg:w-4/12 md:w-4/12 w-8/12">
+            <div
+              data-aos="fade-up"
+              data-aos-offset="200"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="900"
+              className="bg-[#5A50FE] py-5 px-5 rounded-full flex items-center justify-center h-[4.5rem] w-[4.5rem] ">
+              <img src={midSized} alt="" className="w-fit" />
+            </div>
+            <div
+              data-aos="fade-up"
+              data-aos-offset="200"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="900"
+              className="mt-2">
+              <p>Mid Sized Enterprises</p>
+            </div>
+          </div>
 
-              <div className="flex items-center justify-center flex-col lg:w-4/12 md:w-4/12 w-8/12">
-                <div
-                data-aos="fade-up"
-                data-aos-offset="200"
-                data-aos-easing="ease-in-sine"
-                data-aos-duration="1000"
-                className="bg-[#5A50FE] py-5 px-5 rounded-full flex items-center justify-center h-[4.5rem] w-[4.5rem] ">
-                  <img src={largeEnt} alt="" className="w-fit" />
-                </div>
-                <div
-                data-aos="fade-up"
-                data-aos-offset="200"
-                data-aos-easing="ease-in-sine"
-                data-aos-duration="1000"
-                className="mt-2">
-                  <p>Large Corporations</p>
-                </div>
-              </div>
-            
+          <div className="flex items-center justify-center flex-col lg:w-4/12 md:w-4/12 w-8/12">
+            <div
+              data-aos="fade-up"
+              data-aos-offset="200"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="1000"
+              className="bg-[#5A50FE] py-5 px-5 rounded-full flex items-center justify-center h-[4.5rem] w-[4.5rem] ">
+              <img src={largeEnt} alt="" className="w-fit" />
+            </div>
+            <div
+              data-aos="fade-up"
+              data-aos-offset="200"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="1000"
+              className="mt-2">
+              <p>Large Corporations</p>
+            </div>
+          </div>
         </div>
       </div>
       <div className="w-full mt-14 flex justify-center">
         <div className="bg-[#0055BA] py-10 lg:px-10 md:px-10  w-full flex justify-center">
-          <p className="text-center text-white lg:w-10/12 md:w-10/12 w-11/12 lg:leading-normal md:leading-normal leading-loose">
+          <p 
+          data-aos="fade-up"
+          data-aos-offset="200"
+          data-aos-easing="ease-in-sine"
+          data-aos-duration="1000"
+          className="text-center text-white lg:w-10/12 md:w-10/12 w-11/12 lg:leading-normal md:leading-normal leading-loose">
             Our focus is on digital strategies that are driven by data and
             analytics centering on digital discovery, journeys and experience,
             and digital identities to immerse your customers and consumers into
