e4160f5f00c3140e10684b36a1a5ffd962c14f47
Ocan Samuel <ocansamuel@Ocans-MacBook-Pro.local>
2023-06-05 14:17:50 +0300

    scroll effect

--- src/features/home/components/Hero.jsx
+++ src/features/home/components/Hero.jsx
@@ -3,14 +3,12 @@ import thumb from "../../../assets/thumb.png";
 import { BsArrowRight } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import Aos from "aos";
-import 'aos/dist/aos.css'
+import "aos/dist/aos.css";
 
 export const Hero = () => {
-
-  useEffect(()=>{
-
-  }, [])
-
+  useEffect(() => {
+    Aos.init({duration: 1500});
+  }, []);
 
   return (
     <div className="">
@@ -21,20 +19,22 @@ export const Hero = () => {
               <div className="lg:w-8/12 md:w-8/12 w-12/12   ">
                 <div className="lg:w-10/12 w-12/12">
                   <h1
-             
+                    data-aos="fade-up"
                     className="lg:text-4xl text-3xl font-semibold tracking-wide lg:w-9/12 md:w-10/12 w-12/12 leading-tight text-black">
                     Delivering Solutions Powered by{" "}
                     <span className="text-[#0055BA]"> People, Process,</span>{" "}
                     and <span className="text-[#0055BA]"> Technology</span>
                   </h1>
-               
-                  <p  className="mt-5 text-gray-500 text-lg">
+
+                  <p data-aos="slide-up" className="mt-5 text-gray-500 text-lg">
                     We partners with businesses and product teams to
                     conceptualize, design, build and position their digital
                     products and services to scale and succeed in existing or
                     new markets in the digital economy.
                   </p>
                   <Link
+                    data-aos="fade-right" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000"
+
                     to="/contact"
                     className="flex lg:items-start lg:justify-start md:items-start md:justify-start items-center justify-center">
                     <div className=" bg-[#E5E5E5] py-3 px-4 rounded-md flex items-center lg:mt-5 mt-12 ml-0 w-fit gap-2 text-white hover:text-white hover:gap-4 ease-in duration-150  ">
@@ -48,7 +48,11 @@ export const Hero = () => {
                   </Link>
                 </div>
               </div>
-              <div className="lg:w-4/12 md:w-4/12 w-12/12 flex items-center justify-center lg:mt-0 md:mt-0 mt-10">
+              
+              <div  
+                    data-aos="fade-left"
+              
+              className="lg:w-4/12 md:w-4/12 w-12/12 flex items-center justify-center lg:mt-0 md:mt-0 mt-10">
                 <img src={thumb} alt="" />
               </div>
             </div>
